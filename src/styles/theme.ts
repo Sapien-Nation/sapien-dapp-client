fbcf4e523008c80fa5c7816c55a07b7497c6a65c
prince3339 <prince3339@ymail.com>
2021-01-22 22:34:10 +0600

    averta fonts added

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,28 @@
 // mui
 import { createMuiTheme } from '@material-ui/core/styles';
+import { avertaBold, avertaItalic, avertaMedium, avertaRegular } from './fonts';
 
-const theme = createMuiTheme({});
+const theme = createMuiTheme({
+  typography: {
+    htmlFontSize: 10,
+    fontFamily: ['averta', 'Calibri', 'Arial', 'sans-serif'].join(',')
+  },
+  overrides: {
+    MuiCssBaseline: {
+      '@global': {
+        html: {
+          WebkitFontSmoothing: 'auto',
+          fontSize: '62.5%'
+        },
+        '@font-face': ([
+          avertaBold,
+          avertaItalic,
+          avertaMedium,
+          avertaRegular
+        ] as unknown) as React.CSSProperties
+      }
+    }
+  }
+});
 
 export default theme;
