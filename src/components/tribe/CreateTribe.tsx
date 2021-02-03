b31fa8fb62e6a76946a41ff5e80fca21238a818c
Ethaan <ethan.rosanoo@gmail.com>
2021-02-03 09:07:32 -0600

    SPT-585: wip form input style

---
+++ src/components/tribe/CreateTribe.tsx
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+
+//components
+import Dialog from 'components/dialog';
+
+// mui
+import { FormControl, Switch, TextField } from '@material-ui/core';
+
+const defaultValues = {
+  name: '',
+  type: false,
+  unique_identifier: ''
+};
+
+enum Step {
+  TribeSummary,
+  TribeMedia
+}
+
+interface Props {
+  onClose: () => void;
+}
+
+const CreateTribe: React.FC<Props> = ({ onClose }) => {
+  const [step] = useState(Step.TribeSummary);
+
+  const { control, errors, handleSubmit, register /* watch */ } = useForm({
+    defaultValues
+  });
+
+  // TODO handleNext w/Dialog
+  // const handleNext = () => {};
+
+  // TODO handleFormSubmit w/Dialog
+
+  // TODO remove me: debugging
+  // console.log(watch('name'));
+  // console.log(watch('unique_identifier'));
+  // console.log(watch('type'));
+
+  const handleFormSubmit = (data) => {
+    console.log(data);
+  };
+
+  const renderContent = () => {
+    switch (step) {
+      case Step.TribeSummary: {
+        return (
+          <>
+            <FormControl fullWidth variant="outlined">
+              <TextField
+                name="name"
+                label="Name"
+                inputRef={register({ required: true })}
+                placeholder="Name"
+              />
+              {errors.name && <span>This field is required</span>}
+            </FormControl>
+            <FormControl fullWidth variant="outlined">
+              <TextField
+                name="unique_identifier"
+                label="Unique Identifier"
+                inputRef={register({ required: true })}
+              />
+              {errors.unique_identifier && <span>This field is required</span>}
+            </FormControl>
+            <Controller
+              name="type"
+              control={control}
+              defaultValue={defaultValues.type}
+              rules={{ required: true }}
+              render={(props) => (
+                <Switch
+                  color="primary"
+                  onChange={(e) => props.onChange(e.target.checked)}
+                  checked={props.value}
+                  inputProps={{ 'aria-label': 'Tribe Type' }}
+                />
+              )}
+            />
+          </>
+        );
+      }
+      case Step.TribeMedia: {
+        return 'TODO';
+      }
+    }
+  };
+
+  return (
+    <Dialog
+      open
+      title={`New Tribe ${step === Step.TribeSummary ? 1 : 2} / 2`}
+      onClose={onClose}
+    >
+      <form noValidate onSubmit={handleSubmit(handleFormSubmit)}>
+        {renderContent()}
+      </form>
+    </Dialog>
+  );
+};
+
+export default CreateTribe;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -6,67 +6,104 @@ import { avertaBold, avertaItalic, avertaMedium, avertaRegular } from './fonts';
 
 const theme = createMuiTheme({
   palette: {
+    type: 'light',
     common: {
       black: '#333333'
     },
     primary: {
-      main: '#6200EA'
+      light: '#6200EA',
+      main: '#6200EA',
+      dark: '#6200EA'
     }
   },
   typography: {
     htmlFontSize: 10,
     fontFamily: ['averta', 'Calibri', 'Arial', 'sans-serif'].join(',')
-  },
-  overrides: {
-    MuiCssBaseline: {
-      '@global': {
-        html: {
-          WebkitFontSmoothing: 'auto',
-          fontSize: '62.5%'
-        },
-        '@font-face': ([
-          avertaBold,
-          avertaItalic,
-          avertaMedium,
-          avertaRegular
-        ] as unknown) as React.CSSProperties
-      }
-    },
-    MuiDialogTitle: {
-      root: {
-        padding: '5rem 5rem 0 5rem'
-      }
-    },
-    MuiDialogContent: {
-      root: {
-        padding: '1rem 5rem'
-      }
-    },
-    MuiDialogActions: {
-      root: {
-        padding: '1rem 5rem 5rem 5rem'
-      }
-    },
-    MuiButton: {
-      root: {
-        borderRadius: '0.6rem',
-        textTransform: 'none',
-        minHeight: '4rem',
-        minWidth: '8rem',
-        fontWeight: 600
-      },
-      text: {},
-      containedSecondary: {
-        fontWeight: 700
-      }
-    }
   }
 });
+
+// Override default styles
+theme.overrides = {
+  MuiCssBaseline: {
+    '@global': {
+      html: {
+        WebkitFontSmoothing: 'auto',
+        fontSize: '62.5%'
+      },
+      '@font-face': ([
+        avertaBold,
+        avertaItalic,
+        avertaMedium,
+        avertaRegular
+      ] as unknown) as React.CSSProperties
+    }
+  },
+  MuiDialogTitle: {
+    root: {
+      padding: '5rem 5rem 0 5rem'
+    }
+  },
+  MuiDialogContent: {
+    root: {
+      padding: '1rem 5rem'
+    }
+  },
+  MuiDialogActions: {
+    root: {
+      padding: '1rem 5rem 5rem 5rem'
+    }
+  },
+  MuiButton: {
+    root: {
+      borderRadius: '0.6rem',
+      textTransform: 'none',
+      minHeight: '4rem',
+      minWidth: '8rem',
+      fontWeight: 600
+    },
+    text: {},
+    containedSecondary: {
+      fontWeight: 700
+    }
+  },
+  MuiInputLabel: {
+    root: {
+      fontSize: '1.4rem',
+      fontWeight: 600
+    }
+  },
+  MuiInput: {
+    root: {
+      outline: `1px solid transparent`,
+      border: `1px solid #ccc`,
+      padding: theme.spacing(1, 2),
+      borderRadius: '0.6rem',
+      minHeight: '4.6rem',
+      fontSize: '1.4rem',
+      '&$focused': {
+        borderRadius: '0.6rem',
+        border: `2px solid ${theme.palette.primary.main}`,
+        boxSizing: 'border-box',
+        outline: `1px solid ${theme.palette.primary.main}`
+      }
+    },
+    input: {
+      padding: 0
+    }
+    // we don't need `focused: {}` with overrides
+  }
+};
 
 // Global button props
 theme.props = {
   MuiButton: {
     disableElevation: true
+  },
+  MuiInputLabel: {
+    shrink: true
+  },
+  MuiInput: {
+    disableUnderline: true
   }
 };
 
