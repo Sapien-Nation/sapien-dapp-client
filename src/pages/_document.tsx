c2c8fa96049493dcbad109d498cdefcf8af6b805
Ethaan <ethan.rosanoo@gmail.com>
2021-02-04 10:50:05 -0600

    SPT-000: Update to _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,6 @@
 /* istanbul ignore file */
-import { Fragment } from 'react';
+import { Children } from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-// import flush from 'styled-jsx/server';
 
 // mui
 import { ServerStyleSheets as MaterialUiServerStyleSheets } from '@material-ui/core/styles';
@@ -30,22 +29,19 @@ export default class MyDocument extends Document {
 // it's compatible with server-side generation (SSG).
 MyDocument.getInitialProps = async (ctx) => {
   const materialUiSheets = new MaterialUiServerStyleSheets();
-  // const originalRenderPage = ctx.renderPage;
+  const originalRenderPage = ctx.renderPage;
 
-  try {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: [
-        <Fragment key="styles">
-          {initialProps.styles}
-          {materialUiSheets.getStyleElement()}
-          {/* {styledComponentSheet.getStyleElement()} */}
-          {/* {flush() || null} */}
-        </Fragment>
-      ]
-    };
-  } finally {
-    // styledComponentSheet.seal();
-  }
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => materialUiSheets.collect(<App {...props} />)
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  return {
+    ...initialProps,
+    styles: [
+      ...Children.toArray(initialProps.styles),
+      materialUiSheets.getStyleElement()
+    ]
+  };
 };
