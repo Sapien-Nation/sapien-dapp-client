8f8b6e07d23cb99f2f0eeb1abed9bf2726c63616
javier ocanas <javierocanas@gmail.com>
2021-02-14 19:19:45 -0600

    SPT-649 - Rss component [skip actions]

--- src/components/tribe/modals/CreateChannel/index.tsx
+++ src/components/tribe/modals/CreateChannel/index.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
+// types
+import type { Theme } from '@material-ui/core/styles';
+
 // mui
 import {
   Avatar,
@@ -17,11 +20,8 @@ import {
 } from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
 
-//icons
-import { Add as AddIcon } from '@material-ui/icons';
-
 // styles
-import { background, lightGrey, purple, white } from 'styles/colors';
+import { background, darkGrey, lightGrey, purple, white } from 'styles/colors';
 
 // assets
 import {
@@ -30,6 +30,8 @@ import {
   SingleBadge,
   SubscriptionBadge
 } from '../../assets/svg';
+
+import { AddIcon, RssIcon } from 'components/assets/svg';
 
 //components
 import Dialog from 'components/dialog';
@@ -54,7 +56,7 @@ enum Step {
   ChannelRss
 }
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   dropzone: () => ({
     background: background,
     border: `1px dashed ${lightGrey}`,
@@ -86,6 +88,11 @@ const useStyles = makeStyles(() => ({
   },
   badgeLabel: {
     padding: '0 .8rem'
+  },
+  addRssButton: {
+    padding: `${theme.spacing(0.6)}`,
+    marginLeft: `${theme.spacing(1)}`,
+    backgroundColor: `${darkGrey}15`
   }
 }));
 
@@ -334,7 +341,7 @@ const CreateChannelModal: React.FC<Props> = ({ onClose }) => {
                 <Box width="100%">
                   <Button
                     style={{
-                      marginRight: useTheme().spacing(2),
+                      marginRight: theme.spacing(2),
                       color: purple,
                       marginTop: '2rem'
                     }}
@@ -364,7 +371,7 @@ const CreateChannelModal: React.FC<Props> = ({ onClose }) => {
                       aria-label="channel type"
                       style={{ color: theme.palette.infoIcon.main }}
                     >
-                      <AddIcon fontSize="small" />
+                      <AddIcon />
                     </IconButton>
                   );
                 }}
@@ -391,7 +398,7 @@ const CreateChannelModal: React.FC<Props> = ({ onClose }) => {
                       aria-label="channel type"
                       style={{ color: theme.palette.infoIcon.main }}
                     >
-                      <AddIcon fontSize="small" />
+                      <AddIcon />
                     </IconButton>
                   );
                 }}
@@ -410,7 +417,63 @@ const CreateChannelModal: React.FC<Props> = ({ onClose }) => {
       case Step.ChannelRss: {
         return (
           <>
-            <Box>Channel Rss</Box>
+            <FormControl fullWidth>
+              <InputLabel htmlFor="label">Label</InputLabel>
+              <Input
+                fullWidth
+                id="label"
+                inputRef={register({ required: false, maxLength: 36 })}
+                name="label"
+                placeholder="https://"
+              />
+              <Typography
+                style={{ marginTop: theme.spacing(1.5) }}
+                variant="caption"
+              >
+                Enter a URL or{' '}
+                <Typography color="primary" variant="caption">
+                  Browse{' '}
+                </Typography>
+                to import RSS feeds (max 150MB)
+              </Typography>
+              <Box width="100%">
+                <Button
+                  style={{
+                    color: purple,
+                    marginTop: theme.spacing(2)
+                  }}
+                >
+                  + Add More
+                </Button>
+              </Box>
+            </FormControl>
+            <Typography style={{ marginBottom: '2.2rem' }} variant="h5">
+              Popular Feeds
+            </Typography>
+            <Box
+              alignItems="center"
+              bgcolor={background}
+              borderRadius="1rem"
+              display="flex"
+              justifyContent="space-between"
+              padding="1.3rem 1.6rem"
+              width="100%"
+            >
+              <Box alignItems="center" display="flex">
+                <RssIcon />
+                <Typography style={{ marginLeft: '1rem ', fontWeight: 600 }}>
+                  Foodies Feed
+                </Typography>
+              </Box>
+              <IconButton
+                aria-label="add rss"
+                classes={{
+                  root: classes.addRssButton
+                }}
+              >
+                <AddIcon />
+              </IconButton>
+            </Box>
           </>
         );
       }
