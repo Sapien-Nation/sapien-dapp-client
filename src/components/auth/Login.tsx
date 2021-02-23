d7e9b7b5a1f3945f4bed4f3e427f6b701a02cda7
javier ocanas <javierocanas@gmail.com>
2021-02-23 13:46:50 -0600

    SPT-667-LoginLayout + LoginComponent [skip actions]

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -1,29 +1,157 @@
 /* istanbul ignore file */
+
+import { Controller, useFormContext } from 'react-hook-form';
+
 // context
 import { useAuth } from 'context/user';
 
 // next
-import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 // mui
-import { Button } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  Checkbox,
+  CheckboxProps,
+  FormControlLabel,
+  Typography,
+  withStyles
+} from '@material-ui/core';
+
+
+// styles
+import { black, purple } from 'styles/colors';
+
+//components
+import { PasswordInput, TextInput } from 'components/form';
+
+const PurpleCheckbox = withStyles({
+  root: {
+    color: purple,
+    '&$checked': {
+      color: purple,
+    },
+  },
+  checked: {},
+})((props: CheckboxProps) => <Checkbox color="default" {...props} />);
+
 
 const Login = () => {
   const { login } = useAuth();
-  const { push } = useRouter();
+  const { control, register } = useFormContext();
 
   return (
     <>
-      <Button variant="contained" onClick={login}>
-        Login
-      </Button>
+      <Typography
+        style={{
+          margin: '5rem 0',
+          fontSize: '3.2rem'
+        }}
+        variant="h2"
+      >
+        Log in
+      </Typography>
+      <TextInput
+        required
+        label="Email, phone number, or username"
+        maxLength={36}
+        name="username"
+        placeholder="myemailaddress@email.com"
+      />
+      <Controller
+        control={control}
+        name="password"
+        render={({ onChange, name, value }) => (
+          <PasswordInput
+            inputRef={register()}
+            label="Password"
+            name={name}
+            placeholder="Password"
+            style={{ marginBottom: '0' }}
+            value={value}
+            onChange={(e) => onChange(e.target.value)}
+          />
+        )}
+      />
+      <Box
+        alignItems="center"
+        display="flex"
+        justifyContent="space-between"
+        marginBottom="2rem"
+      >
+        <FormControlLabel
+          control={
+            <Controller
+              control={control}
+              name="remember"
+              render={(props) => (
+                <PurpleCheckbox
+                  {...props}
+                  checked={props.value}
+                  onChange={(e) => props.onChange(e.target.checked)}
+                />
+              )}
+            />
+          }
+          label={
+            <Typography
+              style={{
+              fontSize: '1.2rem',
+              fontWeight: 600
+            }}>Remember me</Typography>
+          }
+        />
+        <Link passHref href="/auth#forgot">
+          <Typography
+            style={{
+              color: purple,
+              fontWeight: 600,
+              cursor: 'pointer'
+            }}
+          >
+            Forgot password?
+          </Typography>
+        </Link>
+      </Box>
+
       <Button
+        fullWidth
         color="primary"
         variant="contained"
-        onClick={() => push('/auth#signup', undefined, { shallow: false })}
+        onClick={login}
       >
-        Signup
+        Log In
       </Button>
+      <Box
+        alignItems="center"
+        display="flex"
+        justifyContent="center"
+        marginTop="2rem">
+        <Link passHref href="/auth#signup">
+          <>
+            <Typography
+              style={{
+                fontSize: '1.2rem',
+                color: black,
+                fontWeight: 400,
+                cursor: 'pointer'
+              }}>
+              Don’t have an account?
+            </Typography>
+            <Typography
+              style={{
+                fontSize: '1.2rem',
+                color: black,
+                fontWeight: 700,
+                cursor: 'pointer',
+                marginLeft: '4px'
+              }}>
+              Sign up
+            </Typography>
+          </>
+        </Link>
+      </Box>
     </>
   );
 };
--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -1,14 +1,16 @@
 /* istanbul ignore file */
+
+import { FormProvider, useForm } from 'react-hook-form';
 import { useState } from 'react';
 
 // next
 import { useRouter } from 'next/router';
 
 // mui
-import { CssBaseline } from '@material-ui/core';
+import { Box, CssBaseline, makeStyles } from '@material-ui/core';
 
-// styles
-import { darkPurple } from 'styles/colors';
+// assets
+import { FullLogo } from 'components/assets/svg';
 
 // components
 import { Login, Signup } from 'components/auth';
@@ -18,12 +20,37 @@ enum View {
   Signup,
 }
 
+const useStyles = makeStyles({
+  layout: {
+    display: 'grid',
+    height: '100vh',
+    gridTemplateColumns: '1fr 560px',
+  },
+  image: {
+    backgroundImage: 'url(static/auth.jpg)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: '39rem',
+  },
+});
+
 const AuthPage = () => {
   const { asPath, events } = useRouter();
 
   const [view, setView] = useState(() =>
-    asPath?.includes('#signup') ? View.Signup : View.Login
+    asPath?.includes('#signup') ? View.Signup : View.Login,
   );
+
+  const classes = useStyles();
 
   const renderView = () => {
     switch (view) {
@@ -37,19 +64,23 @@ const AuthPage = () => {
   events?.on('hashChangeComplete', (url) => {
     setView(url.includes('#signup') ? View.Signup : View.Login);
   });
+  const form = 'auth';
+  const methods = useForm();
+
   return (
     <>
       <CssBaseline />
-      <div
-        style={{
-          display: 'grid',
-          height: '100vh',
-          gridTemplateColumns: '1fr 1fr',
-        }}
-      >
-        <div style={{ backgroundColor: darkPurple }}></div>
-        <div>{renderView()}</div>
-      </div>
+      <Box className={classes.layout}>
+        <Box className={classes.image}></Box>
+        <Box className={classes.content}>
+          <Box className={classes.form}>
+            <FullLogo />
+            <FormProvider {...methods}>
+              <form id={form}>{renderView()}</form>
+            </FormProvider>
+          </Box>
+        </Box>
+      </Box>
     </>
   );
 };
