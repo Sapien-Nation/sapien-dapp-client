ec2e2ece3322058738ad71f9cdd946e9095c4ed7
Ethaan <ethan.rosanoo@gmail.com>
2021-03-18 10:24:25 -0500

    SPT-0000: release cleanup

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'jest-tsconfig.json',
+    },
+  },
   transform: {
     '^.+\\.(ts|tsx)?$': 'ts-jest',
     '^.+\\.(js|jsx)$': 'babel-jest',
