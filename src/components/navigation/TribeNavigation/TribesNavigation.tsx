0edd97aabcc35dff0127714a683ebfbd1bc4888d
Ethaan <ethan.rosanoo@gmail.com>
2021-03-26 13:50:37 -0500

    SPT-10026: cleanup

--- src/components/navigation/TribeNavigation/TribesNavigation.tsx
+++ src/components/navigation/TribeNavigation/TribesNavigation.tsx
@@ -1,25 +1,15 @@
-import { useState } from 'react';
-
 // types
 import type { Theme } from '@material-ui/core/styles';
 import type { Tribe } from 'tools/types/tribe';
-
-// next
-import Image from 'next/image';
 
 // constants
 import { NavigationTypes } from 'context/tribes';
 
 // mui
 import {
-  Avatar,
-  Box,
   createStyles,
-  Collapse,
   Drawer,
-  IconButton,
   List,
-  ListItem,
   makeStyles,
   Typography,
 } from '@material-ui/core';
@@ -28,20 +18,15 @@ import {
 import { useNavigation } from 'context/tribes';
 
 // styles
-import {
-  background,
-  black,
-  darkGrey,
-  outline,
-  purple,
-  white,
-} from 'styles/colors';
+import { darkGrey, white } from 'styles/colors';
 
 // assets
-import { AddIcon, ArrowIcon, BadgeStore, TribeName } from '../assets/svg';
+import { BadgeStore, TribeName } from '../assets/svg';
 
-// utils
-import { formatTimestampToRelative } from 'utils/date';
+// components
+import Channels from './Channels';
+import NavigationItem from './NavigationItem';
+import NavigationList from './NavigationList';
 
 const drawerWidth = 228;
 
@@ -62,25 +47,6 @@ const useStyles = makeStyles((theme: Theme) => {
       flexWrap: 'wrap',
       marginTop: `${theme.spacing(1.5)}`,
     },
-    avatar: {
-      color: white,
-      backgroundColor: 'inherit',
-      borderRadius: 15,
-      border: `2px solid ${outline}`,
-      boxSizing: 'content-box',
-      padding: '3px',
-    },
-    avatarImage: {
-      borderRadius: '10px',
-    },
-    collapseButton: {
-      padding: 0,
-    },
-    addChannelButton: {
-      padding: `${theme.spacing(0.6)}`,
-      marginLeft: `${theme.spacing(1)}`,
-      backgroundColor: background,
-    },
   });
 });
 
@@ -91,7 +57,6 @@ interface Props {
 
 const TribeNavigation = ({ createChanel, tribes }: Props) => {
   const [navigation, setNavigation] = useNavigation();
-  const [showChannels, setShowChannels] = useState(true);
   const classes = useStyles();
 
   const tribe: Tribe = tribes.find(({ id }) => id === navigation.main?.id);
@@ -112,18 +77,8 @@ const TribeNavigation = ({ createChanel, tribes }: Props) => {
         className={classes.nav}
         component="nav"
       >
-        <ListItem
-          button
-          disableGutters
-          disableRipple
-          style={{
-            margin: '.65rem',
-            display: 'flex',
-            padding: '1.5rem',
-            backgroundColor:
-              navigation?.secondary === navigation?.main?.id ? purple : white,
-            borderRadius: '1rem',
-          }}
+        <NavigationItem
+          isSelected={navigation?.secondary === navigation?.main?.id}
           onClick={() =>
             setNavigation({
               secondary: navigation?.main?.id,
@@ -131,37 +86,31 @@ const TribeNavigation = ({ createChanel, tribes }: Props) => {
             })
           }
         >
-          <TribeName
-            // @ts-ignore
-            fill={
-              navigation?.secondary === navigation?.main?.id ? white : darkGrey
-            }
-          />
-          <Typography
-            style={{
-              marginLeft: '1.5rem',
-              color:
+          <>
+            <TribeName
+              // @ts-ignore
+              fill={
                 navigation?.secondary === navigation?.main?.id
                   ? white
-                  : darkGrey,
-            }}
-            variant="h5"
-          >
-            {navigation?.main?.name}
-          </Typography>
-        </ListItem>
-        <ListItem
-          button
-          disableGutters
-          disableRipple
-          style={{
-            margin: '.65rem',
-            display: 'flex',
-            padding: '1.5rem',
-            backgroundColor:
-              navigation?.secondary === 'Badge Store' ? purple : white,
-            borderRadius: '1rem',
-          }}
+                  : darkGrey
+              }
+            />
+            <Typography
+              style={{
+                marginLeft: '1.5rem',
+                color:
+                  navigation?.secondary === navigation?.main?.id
+                    ? white
+                    : darkGrey,
+              }}
+              variant="h5"
+            >
+              {navigation?.main?.name}
+            </Typography>
+          </>
+        </NavigationItem>
+        <NavigationItem
+          isSelected={navigation?.type === NavigationTypes.BadgeStore}
           onClick={() =>
             setNavigation({
               secondary: 'Badge Store',
@@ -169,156 +118,35 @@ const TribeNavigation = ({ createChanel, tribes }: Props) => {
             })
           }
         >
-          <BadgeStore
-            // @ts-ignore
-            fill={navigation?.secondary === 'Badge Store' ? white : darkGrey}
-          />
-          <Typography
-            style={{
-              marginLeft: '1.5rem',
-              color: navigation?.secondary === 'Badge Store' ? white : darkGrey,
-            }}
-            variant="h5"
-          >
-            Badge Store
-          </Typography>
-        </ListItem>
-        <ListItem disableGutters component="div">
-          <Box
-            alignItems="center"
-            display="flex"
-            justifyContent="space-between"
-            px={2}
-            py={1}
-            width="100%"
-          >
-            <Box alignItems="center" display="flex">
-              <Typography variant="h5">Channels</Typography>
-              {tribe.permissions.canAddChannel && (
-                <IconButton
-                  aria-label="Create Channel"
-                  classes={{
-                    root: classes.addChannelButton,
-                  }}
-                  onClick={createChanel}
-                >
-                  <AddIcon />
-                </IconButton>
-              )}
-            </Box>
-            <IconButton
-              disableRipple
-              aria-label="show channels"
-              classes={{
-                root: classes.collapseButton,
-              }}
+          <>
+            <BadgeStore
+              fill={
+                navigation?.type === NavigationTypes.BadgeStore
+                  ? white
+                  : darkGrey
+              }
+            />
+            <Typography
               style={{
-                transform: showChannels ? '' : 'rotate(180deg)',
+                marginLeft: '1.5rem',
+                color:
+                  navigation?.type === NavigationTypes.BadgeStore
+                    ? white
+                    : darkGrey,
               }}
-              onClick={() => setShowChannels(!showChannels)}
+              variant="h5"
             >
-              <ArrowIcon />
-            </IconButton>
-          </Box>
-        </ListItem>
-        <Collapse
-          unmountOnExit
-          in={showChannels && Boolean(tribe.channels?.length)}
-          style={{ width: '100%' }}
-          timeout="auto"
+              Badge Store
+            </Typography>
+          </>
+        </NavigationItem>
+        <NavigationList
+          showAddButton={tribe.permissions.canAddChannel}
+          title="Channels"
+          onAdd={createChanel}
         >
-          <List aria-label="Channels list" role="list">
-            {tribe.channels.map((channel) => (
-              <ListItem
-                key={channel.id}
-                button
-                disableGutters
-                disableRipple
-                component="li"
-                onClick={() =>
-                  setNavigation({
-                    secondary: channel.id,
-                    type: NavigationTypes.Channel,
-                  })
-                }
-              >
-                <Box
-                  aria-label={channel.name}
-                  display="flex"
-                  justifyContent="space-between"
-                  role="listitem"
-                  style={{
-                    margin: '0 .65rem',
-                    padding: '1rem',
-                    width: '100%',
-                    color:
-                      navigation?.secondary === channel.id ? white : darkGrey,
-                    backgroundColor:
-                      navigation?.secondary === channel.id ? purple : white,
-                    borderRadius: '1rem',
-                  }}
-                >
-                  <Box display="flex">
-                    <Avatar
-                      alt={channel.name}
-                      classes={{
-                        root: classes.avatar,
-                      }}
-                      variant="square"
-                    >
-                      <Image
-                        alt={channel.name}
-                        className={classes.avatarImage}
-                        height={40}
-                        quality={100}
-                        src={channel.image}
-                        width={40}
-                      />
-                    </Avatar>
-                    <Box
-                      display="flex"
-                      flexDirection="column"
-                      justifyContent="center"
-                      marginLeft={1.2}
-                    >
-                      <Typography
-                        style={{
-                          color:
-                            navigation?.secondary === channel.id
-                              ? white
-                              : black,
-                        }}
-                        variant="body1"
-                      >
-                        {channel.name}
-                      </Typography>
-                      <Typography
-                        style={{
-                          color:
-                            navigation?.secondary === channel.id
-                              ? white
-                              : darkGrey,
-                        }}
-                        variant="body2"
-                      >
-                        {channel.memberCount} members
-                      </Typography>
-                    </Box>
-                  </Box>
-                  <Typography
-                    style={{
-                      color:
-                        navigation?.secondary === channel.id ? white : darkGrey,
-                    }}
-                    variant="body2"
-                  >
-                    {formatTimestampToRelative(channel.lastUpdate)}
-                  </Typography>
-                </Box>
-              </ListItem>
-            ))}
-          </List>
-        </Collapse>
+          <Channels channels={tribe.channels} />
+        </NavigationList>
       </List>
     </Drawer>
   );
