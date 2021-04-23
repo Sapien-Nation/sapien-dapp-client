36589f79654201503b15ede2ee1b2db7d6307fe4
Ethaan <ethan.rosanoo@gmail.com>
2021-04-23 11:43:01 -0500

    SPT-741: PR Cleanup

--- src/components/common/fields/index.ts
+++ src/components/common/fields/index.ts
@@ -5,3 +5,4 @@ export { default as Dropzone } from './Dropzone';
 export { default as PasswordStrengthInput } from './PasswordStrengthInput';
 export { default as Switch } from './Switch';
 export { default as TextInput } from './TextInput';
+export { default as Tooltip } from './Tooltip';
