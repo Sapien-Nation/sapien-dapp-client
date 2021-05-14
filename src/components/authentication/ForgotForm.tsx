49bc00c4584e4f0b54ee94632ea1cfe891ebb87d
javier ocanas <javierocanas@gmail.com>
2021-05-13 22:57:52 -0500

    TRB-28: Forgot pass UI enhancements

--- src/components/authentication/ForgotForm.tsx
+++ src/components/authentication/ForgotForm.tsx
@@ -1,6 +1,5 @@
 /* istanbul ignore file */
 import { useSnackbar } from 'notistack';
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 // next
@@ -10,42 +9,19 @@ import Link from 'next/link';
 import axios from 'api';
 
 // mui
-import {
-  Box,
-  Button,
-  makeStyles,
-  TextField,
-  Typography,
-} from '@material-ui/core';
-
-const useStyles = makeStyles({
-  buttonLink: {
-    minHeight: 0,
-    minWidth: 0,
-    padding: 0,
-    '&:hover': {
-      backgroundColor: 'transparent',
-    },
-  },
-  successButton: {
-    minWidth: '14rem',
-    minHeight: '4.6rem',
-  },
-});
+import { Box, Button, TextField, Typography } from '@material-ui/core';
 
 enum View {
-  Form,
   Success,
 }
-const Forgot = () => {
-  const [view, setView] = useState(View.Form);
+
+interface Props {
+  setView: (view: View) => void;
+}
+
+const Forgot = ({ setView }: Props) => {
   const { handleSubmit, register } = useForm();
   const { enqueueSnackbar } = useSnackbar();
-  const classes = useStyles();
-
-  const { ref, ...rest } = register('username', {
-    required: 'Email is required',
-  });
 
   const onSubmit = async () => {
     try {
@@ -57,110 +33,48 @@ const Forgot = () => {
   };
 
   return (
-    <>
-      {view === View.Success ? (
-        <Box marginTop={4} textAlign="center">
-          <Box marginBottom={4}>
-            <Typography component="h1" variant="h2">
-              Request sent successfully
-            </Typography>
-          </Box>
-          <Box marginBottom={4}>
-            <Typography variant="body2">
-              If the email and username provided match, you will receive
-              instructions to set a new password shortly.
-            </Typography>
-          </Box>
-          <Link href="/login">
-            <Button
-              classes={{ root: classes.successButton }}
-              color="primary"
-              type="submit"
-              variant="contained"
-            >
-              Got it!
-            </Button>
-          </Link>
-          <Box
-            alignItems="center"
-            display="flex"
-            justifyContent="center"
-            marginTop="2rem"
-          >
-            <Typography
-              color="textPrimary"
-              component="span"
-              variant="subtitle2"
-            >
-              Haven’t received an email?
-            </Typography>
-            <Button
-              classes={{ root: classes.buttonLink }}
-              color="inherit"
-              onClick={() => setView(View.Form)}
-            >
-              <Typography
-                component="a"
-                style={{
-                  marginLeft: '4px',
-                }}
-                variant="caption"
-              >
-                Resend
-              </Typography>
-            </Button>
-          </Box>
-        </Box>
-      ) : (
-        <form id="forgot-form" onSubmit={handleSubmit(onSubmit)}>
-          <Box marginY="5rem">
-            <Typography component="h1" variant="h2">
-              Forgotten Password
-            </Typography>
-          </Box>
-          <TextField
-            fullWidth
-            autoComplete="email"
-            inputRef={ref}
-            label="Email, phone number, or username"
-            placeholder="Enter your email, phone number, or username"
-            {...rest}
-          />
+    <form id="forgot-form" onSubmit={handleSubmit(onSubmit)}>
+      <Box marginY="5rem">
+        <Typography component="h1" variant="h2">
+          Forgotten Password
+        </Typography>
+      </Box>
+      <TextField
+        fullWidth
+        InputProps={{ ...register('email') }}
+        autoComplete="email"
+        label="Email, phone number, or username"
+        placeholder="Enter your email, phone number, or username"
+      />
 
-          <Button fullWidth color="primary" type="submit" variant="contained">
-            Send request
-          </Button>
-          <Box
-            alignItems="center"
-            display="flex"
-            justifyContent="center"
-            marginTop="2rem"
-          >
-            <>
-              <Typography
-                color="textPrimary"
-                component="span"
-                variant="subtitle2"
-              >
-                Remembered your password?
-              </Typography>
-              <Link passHref href="/login">
-                <Typography
-                  color="inherit"
-                  component="a"
-                  style={{
-                    marginLeft: '4px',
-                  }}
-                  variant="caption"
-                >
-                  Log in
-                </Typography>
-              </Link>
-            </>
-          </Box>
-        </form>
-      )}
-    </>
+      <Button fullWidth color="primary" type="submit" variant="contained">
+        Send request
+      </Button>
+      <Box
+        alignItems="center"
+        display="flex"
+        justifyContent="center"
+        marginTop="2rem"
+      >
+        <>
+          <Typography color="textPrimary" component="span" variant="subtitle2">
+            Remembered your password?
+          </Typography>
+          <Link passHref href="/login">
+            <Typography
+              color="inherit"
+              component="a"
+              style={{
+                marginLeft: '4px',
+              }}
+              variant="caption"
+            >
+              Log in
+            </Typography>
+          </Link>
+        </>
+      </Box>
+    </form>
   );
 };
 
--- src/pages/forgot.tsx
+++ src/pages/forgot.tsx
@@ -1,6 +1,91 @@
+import { useState } from 'react';
+
+// next
+import Link from 'next/link';
+
+// mui
+import { CssBaseline, Box, Button, Typography } from '@material-ui/core';
+
 // components
 import { ForgotForm } from 'components/authentication';
 
-const ForgotPage = () => <ForgotForm />;
+enum View {
+  Form,
+  Success,
+}
+
+const ForgotPage = () => {
+  const [view, setView] = useState(View.Form);
+
+  return (
+    <>
+      <CssBaseline />
+      <Box
+        alignItems="center"
+        display="flex"
+        height="100vh"
+        justifyContent="center"
+        width="100%"
+      >
+        <Box
+          borderRadius={2.4}
+          boxShadow="0px 25px 50px rgba(52, 46, 62, 0.05);"
+          display="flex"
+          justifyContent="center"
+          padding={5}
+          width={490}
+        >
+          {view === View.Success ? (
+            <Box textAlign="center">
+              <Box marginBottom={3}>
+                <Typography component="h1" variant="h2">
+                  Request sent successfully
+                </Typography>
+              </Box>
+              <Box marginBottom={3}>
+                <Typography variant="body2">
+                  If the email and username provided match, you will receive
+                  instructions to set a new password shortly.
+                </Typography>
+              </Box>
+              <Link href="/login">
+                <Button color="primary" type="submit" variant="contained">
+                  Got it!
+                </Button>
+              </Link>
+              <Box
+                alignItems="center"
+                display="flex"
+                justifyContent="center"
+                marginTop="2rem"
+              >
+                <Typography
+                  color="textPrimary"
+                  component="span"
+                  variant="subtitle2"
+                >
+                  Haven’t received an email?
+                </Typography>
+                <Button color="inherit" onClick={() => setView(View.Form)}>
+                  <Typography
+                    component="a"
+                    style={{
+                      marginLeft: '4px',
+                    }}
+                    variant="caption"
+                  >
+                    Resend
+                  </Typography>
+                </Button>
+              </Box>
+            </Box>
+          ) : (
+            <ForgotForm setView={setView} />
+          )}
+        </Box>
+      </Box>
+    </>
+  );
+};
 
 export default ForgotPage;
