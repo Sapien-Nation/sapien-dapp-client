e8ed9189f971a54b04656f7af1771d364cf0e412
javier ocanas <javierocanas@gmail.com>
2021-05-26 17:00:19 -0500

    TRB-90:  Add TribeNavigation

--- src/components/navigation/Sidebar.tsx
+++ src/components/navigation/Sidebar.tsx
@@ -1,10 +1,12 @@
 // components
 import TribeBar from './TribeBar';
+import TribeNavigation from './TribeNavigation';
 
 const Sidebar = () => {
   return (
     <nav aria-label="Main navigation" style={{ gridArea: 'sidebar' }}>
       <TribeBar />
+      <TribeNavigation />
     </nav>
   );
 };
---
+++ src/components/navigation/TribeNavigation.tsx
@@ -0,0 +1,130 @@
+import { useState } from 'react';
+
+// next
+import Link from 'next/link';
+
+// mui
+import {
+  Box,
+  Collapse,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
+import {
+  Add,
+  ArrowDropUp,
+  ArrowDropDown,
+  Group,
+  ShoppingCart,
+} from '@material-ui/icons';
+
+// styles
+import { darkGrey, gray2, purple, white } from 'styles/colors';
+
+const useStyles = makeStyles(() => ({
+  drawerPaper: {
+    backgroundColor: 'white',
+    borderRight: 'none',
+    left: 72,
+    width: 228,
+  },
+  listItem: {
+    borderRadius: 10,
+    margin: '1rem .5rem',
+    padding: '1rem 1.5rem',
+    width: 'auto',
+  },
+}));
+
+const TribeNavigation = () => {
+  const classes = useStyles();
+  const [showSquares, setShowSquares] = useState(true);
+
+  return (
+    <Drawer
+      anchor="left"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      style={{
+        width: 228,
+      }}
+      variant="permanent"
+    >
+      <List aria-label="Tribe Navigation">
+        <ListItem
+          style={{
+            borderRadius: 10,
+            margin: '1rem .5rem',
+            padding: '1rem 1.5rem',
+            width: 'auto',
+            backgroundColor: purple,
+          }}
+        >
+          <Link href="/client/1">
+            <a style={{ display: 'flex', justifyContent: 'center' }}>
+              <Group fontSize="small" style={{ color: white }} />
+              <Typography
+                style={{
+                  marginLeft: '1.5rem',
+                  color: white,
+                }}
+                variant="captionItem"
+              >
+                Tribe Name
+              </Typography>
+            </a>
+          </Link>
+        </ListItem>
+        <ListItem
+          style={{
+            borderRadius: 10,
+            margin: '1rem .5rem',
+            padding: '1rem 1.5rem',
+            width: 'auto',
+          }}
+        >
+          <Link href="/client/1">
+            <a style={{ display: 'flex', justifyContent: 'center' }}>
+              <ShoppingCart fontSize="small" style={{ color: darkGrey }} />
+              <Typography style={{ marginLeft: 15 }} variant="captionItem">
+                Badge Store
+              </Typography>
+            </a>
+          </Link>
+        </ListItem>
+      </List>
+      <Box alignItems="center" display="flex" paddingX={2}>
+        <Typography variant="captionItem">Squares</Typography>
+        <IconButton
+          style={{
+            backgroundColor: gray2,
+            marginLeft: 10,
+            padding: 2,
+          }}
+        >
+          <Add fontSize="small" />
+        </IconButton>
+        <IconButton
+          style={{ marginLeft: 'auto' }}
+          onClick={() => setShowSquares(!showSquares)}
+        >
+          {showSquares ? <ArrowDropUp /> : <ArrowDropDown />}
+        </IconButton>
+      </Box>
+      <Collapse unmountOnExit in={showSquares} timeout="auto">
+        <List aria-label="Squares">
+          <ListItem style={{ padding: '0 2rem' }}>
+            <Typography variant="body4">#squares</Typography>
+          </ListItem>
+        </List>
+      </Collapse>
+    </Drawer>
+  );
+};
+
+export default TribeNavigation;
