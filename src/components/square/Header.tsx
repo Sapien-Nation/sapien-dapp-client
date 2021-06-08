bfcbf9c9e80ae0966010c09600ec5c68a68bdd5d
Ethaan <ethan.rosanoo@gmail.com>
2021-06-08 12:33:12 -0500

    TRB-0000: PR cleanup

--- src/components/square/Header.tsx
+++ src/components/square/Header.tsx
@@ -4,20 +4,11 @@ import { useRouter } from 'next/router';
 // types
 import { Square } from 'tools/types/square/view';
 
-// next
-import Image from 'next/image';
-
 // mui
-import { Avatar, Box, makeStyles, Typography } from '@material-ui/core';
+import { Avatar, Box, Typography } from '@material-ui/core';
 
 // components
 import { Query } from 'components/common';
-
-const useStyles = makeStyles(() => ({
-  cover: {
-    borderRadius: '1.2rem',
-  },
-}));
 
 interface Props {
   avatar: string;
@@ -26,82 +17,61 @@ interface Props {
 
 const Header = ({ avatar, cover }: Props) => {
   const { query } = useRouter();
-  const classes = useStyles();
 
   return (
-    <Query apiUrl={query.squareid ? `/api/square/${query.squareid}` : ''}>
-      {(square: Square) => {
-        const { description, followersCount, name, membersCount, userName } =
-          square;
-        return (
-          <Box borderRadius="1.6rem" padding={0.8}>
-            <Box>
-              <Box height="20rem" position="relative">
-                <Image
-                  alt={`${name}`}
-                  className={classes.cover}
-                  layout="fill"
-                  objectFit="cover"
-                  src={cover}
+    <Box className="card--rounded" padding={0.8}>
+      <Query apiUrl={query.squareid ? `/api/square/${query.squareid}` : ''}>
+        {(square: Square) => (
+          <>
+            <img
+              alt={square.description}
+              src={cover}
+              style={{ width: '100%', height: 200 }}
+            />
+            <Box paddingBottom={3.5} paddingX={3.2}>
+              <Box display="flex" marginBottom={3.2}>
+                <Avatar
+                  alt={square.name}
+                  src={avatar}
+                  style={{
+                    width: 110,
+                    height: 110,
+                    borderRadius: 16,
+                    border: '3px solid white',
+                    top: '-2.5rem',
+                  }}
+                  variant="rounded"
                 />
-              </Box>
-              <Box
-                paddingX={3}
-                position="relative"
-                style={{
-                  top: '-2.5rem',
-                }}
-              >
                 <Box
-                  alignItems="flex-end"
-                  display="flex"
-                  flexDirection="row"
-                  marginBottom={3}
+                  display="grid"
+                  marginLeft={3}
+                  marginTop={2.8}
+                  style={{ gap: 8 }}
                 >
-                  <Avatar
-                    alt={name}
-                    style={{
-                      width: '11.1rem',
-                      height: '11.1rem',
-                      borderRadius: '1.6rem',
-                      border: `3px solid white`,
-                    }}
-                    variant="rounded"
-                  >
-                    <Image alt={name} height={110} src={avatar} width={110} />
-                  </Avatar>
-                  <Box marginLeft={3}>
-                    <Typography gutterBottom variant="h2">
-                      {name}{' '}
-                      <Typography
-                        gutterBottom
-                        marginLeft={1.5}
-                        variant="buttonMedium"
-                      >
-                        {userName}
-                      </Typography>
+                  <Box alignItems="baseline" display="flex" gap={1.4}>
+                    <Typography variant="h2">{square.name}</Typography>
+                    <Typography color="textSecondary" variant="button">
+                      {square.userName}
                     </Typography>
-                    <Typography
-                      component="span"
-                      marginRight={1.5}
-                      variant="body4"
-                    >
-                      {followersCount} Followers
+                  </Box>
+                  <Box alignItems="baseline" display="flex" gap={1.4}>
+                    <Typography color="textSecondary" variant="button">
+                      {square.followersCount} Followers
                     </Typography>
-                    <Typography component="span" variant="body4">
-                      {membersCount} Members
+                    <Typography color="textSecondary" variant="button">
+                      {square.membersCount} Members
                     </Typography>
                   </Box>
                 </Box>
-                <Typography color="textSecondary" variant="body2">
-                  {description}
-                </Typography>
               </Box>
+              <Typography color="textSecondary" variant="body2">
+                {square.description}
+              </Typography>
             </Box>
-          </Box>
-        );
-      }}
-    </Query>
+          </>
+        )}
+      </Query>
+    </Box>
   );
 };
 
