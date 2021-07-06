e1524a97f50aa82130df748532f252534fbe3839
javier ocanas <javierocanas@gmail.com>
2021-07-06 16:31:42 -0500

    TKN-112 : UI - Wallet Transaction History Modal

--- src/components/wallet/Transactions.tsx
+++ src/components/wallet/Transactions.tsx
@@ -11,10 +11,11 @@ import { mockTransaction } from 'tools/mocks/balance/transactions';
 import { Query } from 'components/common';
 
 // mui
-import { Avatar, Box, Typography } from '@material-ui/core';
+import { Avatar, Badge, Box, IconButton, Typography } from '@material-ui/core';
+import { ExitToApp as ExitToAppIcon } from '@material-ui/icons';
 
 // styles
-import { neutral, primary } from 'styles/colors';
+import { green, neutral, red } from 'styles/colors';
 
 // utils
 import { formatTimestampToRelative } from 'utils/date';
@@ -24,7 +25,7 @@ const Transactions = () => {
     <Box padding={2}>
       <Query
         api="/api/v3/balance/transactions"
-        options={{ fetcher: () => [mockTransaction()] }}
+        options={{ fetcher: () => [mockTransaction(), mockTransaction()] }}
       >
         {(transactions: Array<Transaction>) => (
           <>
@@ -34,13 +35,45 @@ const Transactions = () => {
                 alignItems="center"
                 display="flex"
                 justifyContent="space-between"
+                marginBottom={1}
                 style={{ gap: 20 }}
               >
-                <Avatar style={{ backgroundColor: '#FFECF2' }}>
-                  <TransactionIcon />
-                </Avatar>
+                <Badge
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  badgeContent={
+                    <Avatar
+                      alt="Slowpoke"
+                      src="/fixtures/normal/slowpoke.jpg"
+                      style={{
+                        width: 20,
+                        height: 20,
+                        border: '2px solid #FFF',
+                      }}
+                    />
+                  }
+                  overlap="circle"
+                >
+                  <Avatar
+                    style={{
+                      backgroundColor: index === 1 ? red[100] : green[100],
+                    }}
+                  >
+                    <TransactionIcon
+                      style={{
+                        fill: index === 1 ? red[700] : green[700],
+                        transform: `${index === 0 && 'rotate(180deg)'}`,
+                      }}
+                    />
+                  </Avatar>
+                </Badge>
                 <Box display="flex" flexDirection="column">
-                  <Typography style={{ lineHeight: 1.4 }} variant="button">
+                  <Typography
+                    style={{ lineHeight: 1.4, textTransform: 'capitalize' }}
+                    variant="button"
+                  >
                     {transactions.info.type}
                   </Typography>
                   <Typography
@@ -52,39 +85,38 @@ const Transactions = () => {
                     variant="button"
                   >
                     {formatTimestampToRelative(transactions.updatedAt)}
+                    {`, `}
+                    {transactions.info.status}
                   </Typography>
                 </Box>
-                <Box display="flex" flexDirection="column" marginLeft="auto">
-                  <Typography style={{ lineHeight: 1.4 }} variant="button">
-                    {transactions.info.amount}
-                  </Typography>
-                  <Typography
-                    style={{
-                      lineHeight: 1.4,
-                      color: neutral[500],
-                      fontWeight: 400,
-                    }}
-                    variant="button"
-                  >
-                    ${transactions.info.amount}
-                  </Typography>
+                <Box alignItems="center" display="flex" marginLeft="auto">
+                  <Box display="flex" flexDirection="column">
+                    <Typography style={{ lineHeight: 1.4 }} variant="button">
+                      {transactions.info.amount} SPN
+                    </Typography>
+                    <Typography
+                      style={{
+                        lineHeight: 1.4,
+                        color: neutral[500],
+                        fontWeight: 400,
+                      }}
+                      variant="button"
+                    >
+                      ${transactions.info.amount}
+                    </Typography>
+                  </Box>
+                  <IconButton aria-label="go to tx">
+                    <ExitToAppIcon
+                      fontSize="small"
+                      style={{ color: neutral[400] }}
+                    />
+                  </IconButton>
                 </Box>
               </Box>
             ))}
           </>
         )}
       </Query>
-      <Typography
-        style={{
-          fontSize: 14,
-          textAlign: 'center',
-          color: primary[800],
-          fontWeight: 600,
-          marginTop: 1,
-        }}
-      >
-        See All
-      </Typography>
     </Box>
   );
 };
