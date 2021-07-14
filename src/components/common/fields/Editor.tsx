c3cd2dccba6bd35f8f68888869c48f3d46493279
Moises <moises@sapien.network>
2021-07-13 22:55:27 -0500

    TRB-0000: Editor component fixes

--- src/components/common/fields/Editor.tsx
+++ src/components/common/fields/Editor.tsx
@@ -1,35 +1,180 @@
-import { Editable, Slate } from 'slate-react';
+import { Editable, Slate, withReact } from 'slate-react';
+import { useSnackbar } from 'notistack';
+import { withHistory } from 'slate-history';
+import { createEditor, Descendant, Transforms } from 'slate';
+import React, { useRef, useState } from 'react';
+
+// mui
+import { Box, IconButton } from '@material-ui/core';
+import { ImageOutlined, Send } from '@material-ui/icons';
 
 // utils
 import { Element, Leaf } from 'utils/slate';
 
+// styles
+import { neutral, primary } from 'styles/colors';
+
+// utils
+import {
+  composeSlateHighOrderFns,
+  withImages,
+  withLinks,
+  withShortcuts,
+} from 'utils/slate';
+
 interface Props {
-  editor: any;
   editorProps: any;
-  value: any;
+  isSubmitting: boolean;
   onChange: (editor: any) => void;
 }
 
-const Editor = ({ editor, editorProps = {}, value, onChange }: Props) => {
+const initialEditorValue = [
+  {
+    type: 'paragraph',
+    children: [{ text: '' }],
+  },
+];
+
+const Editor = ({ editorProps = {}, isSubmitting, onChange }: Props) => {
+  const [data, setData] = useState<Array<Descendant>>(initialEditorValue);
+  const [editor] = useState(() =>
+    composeSlateHighOrderFns(
+      withImages,
+      withShortcuts,
+      withLinks,
+      withHistory,
+      withReact
+    )(createEditor())
+  );
+
+  const imageRef = useRef(null);
+  const { enqueueSnackbar } = useSnackbar();
+
+  const hasContent = () => {
+    const position = editor.selection?.anchor;
+    const offset = position?.offset;
+    const path = position?.path[0];
+
+    if (editor.selection && (offset !== 0 || path !== 0)) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const removeImage = () => {
+    //TODO remove image from API
+    // return editor
+  };
+
+  const handleUploadImage = async (event: any) => {
+    try {
+      setTimeout(() => {
+        if (event.target.files && event.target.files.length > 0) {
+          const file = event.target.files[0];
+          const newFile = Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          });
+          const url = newFile.preview;
+
+          const image = {
+            type: 'image',
+            url,
+            children: [{ text: '' }],
+            removeMethod: removeImage,
+          };
+          Transforms.insertNodes(editor, image);
+
+          enqueueSnackbar('Image added successfully', {
+            variant: 'success',
+            anchorOrigin: {
+              vertical: 'bottom',
+              horizontal: 'center',
+            },
+          });
+
+          return editor;
+        }
+      }, 2000);
+    } catch (error) {
+      enqueueSnackbar(error, {
+        variant: 'error',
+        anchorOrigin: {
+          vertical: 'bottom',
+          horizontal: 'center',
+        },
+      });
+    }
+  };
+
+  const onChangeEditor = (data: any) => {
+    setData(data);
+    onChange(data);
+  };
+
   return (
-    <Slate editor={editor} value={value} onChange={onChange}>
-      {/* <Toolbar>
-        <MarkButton format="bold" />
-        <MarkButton format="italic" />
-        <MarkButton format="underline" />
-        <MarkButton format="code" />
-      </Toolbar> */}
-      <Editable
-        spellCheck
-        renderElement={(props) => <Element {...props} />}
-        renderLeaf={(props) => <Leaf {...props} />}
-        style={{
-          padding: '0.7rem 1.5rem',
-          width: '100%',
-        }}
-        {...editorProps}
-      />
-    </Slate>
+    <Box
+      borderRadius={16}
+      display="flex"
+      flexDirection={hasContent() ? 'column' : 'row'}
+      padding={0.5}
+      style={{ backgroundColor: neutral[50] }}
+      width="100%"
+    >
+      <Slate editor={editor} value={data} onChange={onChangeEditor}>
+        <Editable
+          spellCheck
+          renderElement={(props) => <Element {...props} />}
+          renderLeaf={(props) => <Leaf {...props} />}
+          style={{
+            padding: '0.7rem 1.5rem',
+            width: '100%',
+          }}
+          {...editorProps}
+        />
+      </Slate>
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        padding={hasContent() ? 0.5 : 0}
+      >
+        <IconButton
+          style={{
+            borderRadius: 10,
+            height: 40,
+            width: 40,
+          }}
+          onClick={() => imageRef.current.click()}
+        >
+          <ImageOutlined fontSize="small" style={{ color: neutral[400] }} />
+        </IconButton>
+        <input
+          ref={imageRef}
+          hidden
+          accept="image/*"
+          id="upload-image"
+          type="file"
+          onChange={handleUploadImage}
+        />
+        <IconButton
+          disabled={isSubmitting}
+          style={{
+            backgroundColor: hasContent() ? primary[800] : '',
+            borderRadius: 10,
+            height: 40,
+            width: 40,
+          }}
+          type="submit"
+        >
+          <Send
+            fontSize="small"
+            style={{
+              color: hasContent() ? '#fff' : neutral[400],
+            }}
+          />
+        </IconButton>
+      </Box>
+    </Box>
   );
 };
 
--- src/components/content/CreateContentForm.tsx
+++ src/components/content/CreateContentForm.tsx
@@ -1,128 +1,43 @@
-import { useRef, useState } from 'react';
-import { withHistory } from 'slate-history';
+import { useState } from 'react';
 import { useSnackbar } from 'notistack';
-import { createEditor, Descendant, Transforms } from 'slate';
-import { withReact } from 'slate-react';
+import { Controller, useForm } from 'react-hook-form';
 
 // components
 import { Editor } from 'components/common';
 
 // mui
-import { Avatar, Box, IconButton } from '@material-ui/core';
-import { ImageOutlined, Send } from '@material-ui/icons';
+import { Avatar, Box } from '@material-ui/core';
 
 // types
 import type { User } from 'tools/types/user';
-
-// styles
-import { neutral, primary } from 'styles/colors';
-
-// utils
-import {
-  clearEditor,
-  composeSlateHighOrderFns,
-  withImages,
-  withLinks,
-  withShortcuts,
-} from 'utils/slate';
 
 interface Props {
   user: User;
   onSubmit: (values: any) => void;
 }
 
-const initialEditorValue = [
-  {
-    type: 'paragraph',
-    children: [{ text: '' }],
-  },
-];
-
 const CreateContentForm = ({ user, onSubmit }: Props) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const imageRef = useRef(null);
 
-  const [editor] = useState(() =>
-    composeSlateHighOrderFns(
-      withImages,
-      withShortcuts,
-      withLinks,
-      withHistory,
-      withReact
-    )(createEditor())
-  );
+  const { control, handleSubmit, setValue, watch } = useForm({
+    defaultValues: {
+      content: '',
+    },
+  });
+
   // @ts-ignore
-  const [data, setData] = useState<Array<Descendant>>(initialEditorValue);
   const { enqueueSnackbar } = useSnackbar();
+  const [content] = watch(['content']);
 
-  const hasContent = () => {
-    const position = editor.selection?.anchor;
-    const offset = position?.offset;
-    const path = position?.path[0];
+  const setFormValue = (data: any) => setValue('content', data);
 
-    if (editor.selection && (offset !== 0 || path !== 0)) {
-      return true;
-    }
-
-    return false;
-  };
-
-  const removeImage = () => {
-    //TODO remove image from API
-    // return editor
-  };
-
-  const handleUploadImage = async (event: any) => {
-    try {
-      setTimeout(() => {
-        if (event.target.files && event.target.files.length > 0) {
-          const file = event.target.files[0];
-          const newFile = Object.assign(file, {
-            preview: URL.createObjectURL(file),
-          });
-          const url = newFile.preview;
-
-          const image = {
-            type: 'image',
-            url,
-            children: [{ text: '' }],
-            removeMethod: removeImage,
-          };
-          Transforms.insertNodes(editor, image);
-
-          enqueueSnackbar('Image added successfully', {
-            variant: 'success',
-            anchorOrigin: {
-              vertical: 'bottom',
-              horizontal: 'center',
-            },
-          });
-
-          return editor;
-        }
-      }, 2000);
-    } catch (error) {
-      enqueueSnackbar(error, {
-        variant: 'error',
-        anchorOrigin: {
-          vertical: 'bottom',
-          horizontal: 'center',
-        },
-      });
-    }
-  };
-
-  const onSubmitForm = async (event) => {
+  const onSubmitForm = async () => {
     setIsSubmitting(true);
     try {
-      event.preventDefault();
-
-      await onSubmit(data);
-
-      clearEditor(editor);
+      await onSubmit(content);
 
       // @ts-ignore
-      setData(initialEditorValue);
+      // setData(initialEditorValue);
     } catch (err) {
       enqueueSnackbar(err.message);
     }
@@ -130,67 +45,27 @@ const CreateContentForm = ({ user, onSubmit }: Props) => {
   };
 
   return (
-    <form onSubmit={onSubmitForm}>
+    <form onSubmit={handleSubmit(onSubmitForm)}>
       <Box alignItems="flex-end" display="flex" style={{ gap: 10 }}>
         <Avatar src={user.avatar}>{user.username[0].toUpperCase()}</Avatar>
-        <Box
-          borderRadius={16}
-          display="flex"
-          flexDirection={hasContent() ? 'column' : 'row'}
-          padding={0.5}
-          style={{ backgroundColor: neutral[50] }}
-          width="100%"
-        >
-          <Editor
-            editor={editor}
-            editorProps={{
-              placeholder: `What’s on your mind, ${user.username}?`,
-            }}
-            value={data}
-            onChange={setData}
-          />
-          <Box
-            display="flex"
-            justifyContent="flex-end"
-            padding={hasContent() ? 0.5 : 0}
-          >
-            <IconButton
-              style={{
-                borderRadius: 10,
-                height: 40,
-                width: 40,
-              }}
-              onClick={() => imageRef.current.click()}
-            >
-              <ImageOutlined fontSize="small" style={{ color: neutral[400] }} />
-            </IconButton>
-            <input
-              ref={imageRef}
-              hidden
-              accept="image/*"
-              id="upload-image"
-              type="file"
-              onChange={handleUploadImage}
-            />
-            <IconButton
-              disabled={isSubmitting}
-              style={{
-                backgroundColor: hasContent() ? primary[800] : '',
-                borderRadius: 10,
-                height: 40,
-                width: 40,
-              }}
-              type="submit"
-            >
-              <Send
-                fontSize="small"
-                style={{
-                  color: hasContent() ? '#fff' : neutral[400],
-                }}
-              />
-            </IconButton>
-          </Box>
-        </Box>
+        <Controller
+          control={control}
+          name="content"
+          render={({ field }) => {
+            return (
+              <>
+                <Editor
+                  editorProps={{
+                    placeholder: `What’s on your mind, ${user.username}?`,
+                  }}
+                  isSubmitting={isSubmitting}
+                  onChange={setFormValue}
+                  {...field}
+                />
+              </>
+            );
+          }}
+        />
       </Box>
     </form>
   );
