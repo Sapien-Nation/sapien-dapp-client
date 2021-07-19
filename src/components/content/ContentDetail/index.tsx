b1f4dc535a473c39364f158c70878bc1e8ed71b8
Alex de la Rosa <alecksdelarosa@gmail.com>
2021-07-19 13:09:32 -0500

    PDT-434: Get Post Detail fix

--- src/components/content/ContentDetail/index.tsx
+++ src/components/content/ContentDetail/index.tsx
@@ -10,9 +10,6 @@ import { ContentDetailSkeleton, Query } from 'components/common';
 // types
 import type { Content as ContentType } from 'tools/types/content';
 
-// mocks
-import { mockContent } from 'tools/mocks/content';
-
 // mui
 import { Box } from '@material-ui/core';
 
@@ -34,14 +31,8 @@ const ContentDetail = ({ contentID, mutateReply }: Props) => {
       style={{ gap: 22 }}
     >
       <Query
-        api={`/post/${contentID}`}
+        api={`/api/v3/post/${contentID}`}
         loader={<ContentDetailSkeleton />}
-        options={{
-          fetcher: () =>
-            mockContent({
-              data: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur scelerisque velit et orci pulvinar, ut viverra nibh pretium. Suspendisse ultrices nisi metus, eu suscipit magna commodo non. Donec consequat diam quis placerat accun fusce. Porttitor ante a interdum aliquam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Etiam consectetur bibendum turpis vitae suscipit. Nam eget lorem tempor, ornare mi vitae, tempus enim. Donec maximus tortor in dolor ullamcorper, in lacinia libero eleifend. Nam convallis quam lacus, non feugiat sapien sollicitudin quis. Donec lobortis varius orci non laoreet. Curabitur finibus dui vel sodales hendrerit. Aenean eu ligula mi. Nunc sagittis sapien id tellus efficitur maximus. Fusce risus libero, consequat sed sapien in, dapibus rutrum turpis. Nam pretium sapien non sem porttitor, et rhoncus enim accumsan. In lacus ipsum, bibendum at faucibus nec, fringilla a dolor. Proin sit amet enim vitae quam eleifend vulputate.',
-            }),
-        }}
       >
         {(content: ContentType) => (
           <>
