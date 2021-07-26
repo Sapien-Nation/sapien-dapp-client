355127f13b9c45176a69fa4af0ce75f04d244634
Sabbir <prince3339@ymail.com>
2021-07-26 15:53:09 +0600

    ContentDetail loader style updates

--- src/components/common/skeletons/ContentDetailSkeleton.tsx
+++ src/components/common/skeletons/ContentDetailSkeleton.tsx
@@ -6,22 +6,14 @@ import { Skeleton } from '@material-ui/lab';
 import PostComposerSkeleton from './PostComposerSkeleton';
 
 const ContentDetailSkeleton = () => (
-  <Box
-    className="card--rounded-white"
-    display="grid"
-    paddingX={3}
-    paddingY={3.6}
-    style={{ gap: 30 }}
-  >
+  <Box className="card--rounded-white" display="grid" style={{ gap: 30 }}>
     <Box alignItems="center" display="flex" style={{ gap: 10 }}>
       <Skeleton animation="wave" height={32} variant="circle" width={32} />
       <Skeleton animation="wave" height={20} variant="rect" width="100%" />
     </Box>
     <Skeleton animation="wave" height={166} variant="rect" />
     <Skeleton animation="wave" height={40} variant="rect" width="100%" />
-    <Box marginX={-3}>
-      <Skeleton animation="wave" height={1} variant="rect" width="100%" />
-    </Box>
+    <Box borderColor="grey.100" borderTop={1} marginX={-3} />
     <PostComposerSkeleton />
   </Box>
 );
