ac4d2046e4ea4e4d6fa8529cbfc455af9add86a7
Ethaan <ethan.rosanoo@gmail.com>
2021-07-29 10:34:15 -0500

    fixing build

--- src/stories/CreateContentForm.stories.tsx
+++ src/stories/CreateContentForm.stories.tsx
@@ -18,7 +18,12 @@ export default StoryMeta;
 export const ComposerStory = () => {
   return (
     <div>
-      <CreateContentForm user={user} onSubmit={() => {}} />
+      <CreateContentForm
+        setIsCreating={() => {}}
+        squareID="1"
+        user={user}
+        onSave={() => {}}
+      />
     </div>
   );
 };
