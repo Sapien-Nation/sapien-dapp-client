fdfa9754f40fdec2ed384b77a706556d5bc35977
javier ocanas <javierocanas@gmail.com>
2021-08-03 12:33:21 -0500

    TKN-133: UI SPN tab (Receivers screen)

--- src/components/wallet/Spn/Receivers.tsx
+++ src/components/wallet/Spn/Receivers.tsx
@@ -8,19 +8,18 @@ import SearchInput from '../shared/SearchInput';
 import {
   Avatar,
   Box,
+  Button,
   Input,
   InputAdornment,
-  IconButton,
-  Typography,
   makeStyles,
+  Typography,
 } from '@material-ui/core';
-import { ArrowBack as ArrowBackIcon } from '@material-ui/icons';
 
 // styles
 import { red, neutral } from 'styles/colors';
 
 // utils
-import { formatSpn, formatSpnToUsd } from 'utils/spn';
+import { formatSpn } from 'utils/spn';
 
 // assets
 import { Spn as SpnIcon } from 'assets';
@@ -86,7 +85,7 @@ export const ReceiverItem = ({
       className={classes.receiverItem}
       display="flex"
       marginTop={1}
-      padding={1}
+      padding={0.5}
       style={{
         cursor: 'pointer',
       }}
@@ -124,7 +123,6 @@ export const ReceiverItem = ({
 };
 
 const NumberFormatInput = ({ name, onChange, ...rest }) => {
-  console.log('HOLA!');
   delete rest.inputRef;
   return (
     <NumberFormat
@@ -151,108 +149,141 @@ const Receivers = ({ setShowTabsMenu, setCurrentReceiver }) => {
   const watchReceive = watch('receive');
   const classes = useStyles();
   return (
-    <div
-      style={{
-        padding: '2rem 2.4rem',
-        height: '100%',
-        display: 'grid',
-        gridTemplateRows: '42px 50px 1fr 188px',
-      }}
+    <Box
+      display="flex"
+      flexDirection="column"
+      height="calc(100% - 90px)"
+      justifyContent="space-between"
     >
-      <div style={{ display: 'flex', marginBottom: '1rem' }}>
-        <IconButton
-          aria-label="go back"
-          style={{
-            padding: 0,
-            marginRight: 6,
-            marginBottom: 10,
-          }}
+      <div
+        style={{
+          height: '100%',
+          display: 'grid',
+          margin: '0 2.4rem',
+          gridTemplateRows: '32px 50px 1fr 200px',
+        }}
+      >
+        <div style={{ display: 'flex', marginBottom: '1rem' }}>
+          <Typography variant="button">Receiver</Typography>
+        </div>
+        <SearchInput
+          ItemComponent={ReceiverItem}
+          list={mockList}
+          setCurrentReceiver={setCurrentReceiver}
+        />
+        <div>
+          <Typography
+            style={{
+              color: neutral[500],
+              textTransform: 'uppercase',
+              fontWeight: 700,
+              lineHeight: 1,
+            }}
+            variant="overline"
+          >
+            will receive
+          </Typography>
+          <Box
+            bgcolor={neutral[50]}
+            borderRadius={10}
+            marginTop={1.5}
+            padding={2}
+          >
+            <Box alignItems="center" display="flex">
+              <SpnIcon style={{ marginRight: 10 }} />
+              <Input
+                classes={{
+                  root: classes.amountInput,
+                  focused: classes.amountInputFocus,
+                }}
+                endAdornment={
+                  <InputAdornment
+                    position="end"
+                    style={{ color: neutral[700] }}
+                  >
+                    SPN{' '}
+                  </InputAdornment>
+                }
+                inputComponent={NumberFormatInput as any}
+                inputProps={{
+                  ...register('receive'),
+                  autoComplete: 'receive',
+                  maxLength: 8,
+                }}
+                placeholder="0"
+                style={{
+                  backgroundColor: 'transparent',
+                  fontSize: 22,
+                  fontWeight: 700,
+                  minHeight: 30,
+                  padding: 0,
+                  width: 110 + String(watchReceive)?.length * 16,
+                }}
+              />
+            </Box>
+            <Box display="flex" justifyContent="space-between" marginTop={1}>
+              <Typography
+                style={{ color: red[700], fontWeight: 100 }}
+                variant="caption"
+              >
+                {isDirty && watchReceive < minSpn ? 'Minimum 100 SPN' : null}
+              </Typography>
+              <Typography
+                style={{
+                  fontWeight: 100,
+                  color: userBalance < watchReceive ? red[700] : neutral[500],
+                }}
+                variant="caption"
+              >
+                Remaining balance{' '}
+                {watchReceive <= userBalance
+                  ? formatSpn(userBalance - watchReceive)
+                  : formatSpn(userBalance)}{' '}
+                SPN
+              </Typography>
+            </Box>
+          </Box>
+          <Box
+            bgcolor={neutral[50]}
+            borderRadius={10}
+            marginTop={1}
+            padding={1}
+            textAlign="center"
+          >
+            <Typography
+              style={{
+                fontWeight: 100,
+                color: neutral[500],
+                textAlign: 'center',
+              }}
+              variant="caption"
+            >
+              Current exchange rate 1 SPN = $0.529
+            </Typography>
+          </Box>
+        </div>
+      </div>
+      <Box
+        borderTop="1px solid #EDEEF0"
+        display="flex"
+        flexWrap="wrap"
+        justifyContent="center"
+        paddingX={2.4}
+        paddingY={2}
+      >
+        <Button
+          fullWidth
+          color="primary"
+          type="submit"
+          variant="contained"
           onClick={() => {
             setShowTabsMenu(true);
           }}
         >
-          <ArrowBackIcon fontSize="small" style={{ color: neutral[700] }} />
-        </IconButton>
-        <Typography variant="button">Receiver</Typography>
-      </div>
-      <SearchInput
-        ItemComponent={ReceiverItem}
-        list={mockList}
-        setCurrentReceiver={setCurrentReceiver}
-      />
-      <Box bgcolor={neutral[50]} borderRadius={10} padding={2}>
-        <Typography
-          style={{
-            color: neutral[500],
-            textTransform: 'uppercase',
-            fontWeight: 700,
-            lineHeight: 1,
-          }}
-          variant="overline"
-        >
-          will receive
-        </Typography>
-        <br />
-        <SpnIcon style={{ marginRight: 10 }} />
-        <Input
-          classes={{
-            root: classes.amountInput,
-            focused: classes.amountInputFocus,
-          }}
-          endAdornment={
-            <InputAdornment position="end" style={{ color: neutral[700] }}>
-              SPN{' '}
-              <small
-                style={{
-                  marginLeft: 10,
-                  color: neutral[500],
-                  fontSize: 12,
-                  fontWeight: 100,
-                }}
-              >
-                {formatSpnToUsd(watchReceive)}
-              </small>
-            </InputAdornment>
-          }
-          inputComponent={NumberFormatInput as any}
-          inputProps={{
-            ...register('receive'),
-            autoComplete: 'receive',
-            maxLength: 8,
-          }}
-          placeholder="0"
-          style={{
-            backgroundColor: 'transparent',
-            fontSize: 22,
-            fontWeight: 700,
-            minHeight: 30,
-            padding: 0,
-            width: 140 + String(watchReceive)?.length * 16,
-          }}
-        />
-        <Box display="flex" justifyContent="space-between" marginTop={3}>
-          <Typography
-            style={{ color: red[700], fontWeight: 100 }}
-            variant="caption"
-          >
-            {isDirty && watchReceive < minSpn ? 'Minimum 100 SPN' : null}
-          </Typography>
-          <Typography
-            style={{
-              fontWeight: 100,
-              color: userBalance < watchReceive ? red[700] : neutral[500],
-            }}
-            variant="caption"
-          >
-            Balance{' '}
-            {watchReceive <= userBalance
-              ? formatSpn(userBalance - watchReceive)
-              : formatSpn(userBalance)}{' '}
-            SPN
-          </Typography>
-        </Box>
+          Send SPN
+        </Button>
       </Box>
-    </div>
+    </Box>
   );
 };
 
