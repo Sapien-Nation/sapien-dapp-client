5276b03174019a237d3507132727382f77f1a9cf
mtabasco <matabasco@gmail.com>
2021-08-24 03:18:19 +0200

    fetch transactions from api

--- src/api/spn-wallet/wallet/config.ts
+++ src/api/spn-wallet/wallet/config.ts
@@ -7,6 +7,7 @@ const getConfig = (isMainnet: boolean) =>
         SPN_TOKEN_ADDRESS: '0x20f7a3ddf244dc9299975b4da1c39f8d5d75f05a', // Mainnet SPN
         BADGE_STORE_ADDRESS: '',
         BICONOMY_API_KEY: '',
+        EXPLORER_BASE_URL: 'https://polygonscan.com/tx/',
       }
     : {
         MAINNET_NETWORK_ID: 5,
@@ -15,5 +16,6 @@ const getConfig = (isMainnet: boolean) =>
         SPN_TOKEN_ADDRESS: '0x2d280CeF1B0Ab6E78a700824Ebe368C2E1B00BEd', // Mumbai SPN
         BADGE_STORE_ADDRESS: '0xab221c69D8EEcF6aC7944efD4589DA206AE1046C',
         BICONOMY_API_KEY: 'tYSKReKvQ.c2fbc08c-3991-49b8-8ed8-cb945b0e55fe',
+        EXPLORER_BASE_URL: 'https://mumbai.polygonscan.com/tx/',
       };
 export default getConfig;
--- src/components/wallet/Transactions.tsx
+++ src/components/wallet/Transactions.tsx
@@ -1,80 +1,125 @@
+import useSWR from 'swr';
+
 // assets
 import { Transaction as TransactionIcon } from 'assets';
 
 // types
-import type { Transaction } from 'tools/types/wallet/transactions';
-
-// mocks
-import { mockTransaction } from 'tools/mocks/wallet/transactions';
+import {
+  Transaction,
+  TransactionStatus,
+  TransactionType,
+} from 'tools/types/wallet/transactions';
 
 // components
-import { Query } from 'components/common';
+import { WalletSkeleton } from 'components/common';
+
+// context
+import { useAuth } from 'context/user';
 
 // mui
-import { Avatar, Badge, Box, IconButton, Typography } from '@material-ui/core';
+import {
+  Avatar,
+  Badge,
+  Box,
+  IconButton,
+  Link,
+  Typography,
+} from '@material-ui/core';
 import { ExitToApp as ExitToAppIcon } from '@material-ui/icons';
+
+// api
+import { tokensInstance } from 'api';
+import getConfig from 'api/spn-wallet/wallet/config';
 
 // styles
 import { green, neutral, red } from 'styles/colors';
 
 // utils
-// import { formatTimestampToRelative } from 'utils/date';
+import { formatTimestampToRelative } from 'utils/date';
 
 const Transactions = () => {
-  return (
-    <Box padding={2}>
-      <Query
-        api="/api/v3/balance/transactions"
-        options={{ fetcher: () => [mockTransaction(), mockTransaction()] }}
-      >
-        {(transactions: Array<Transaction>) => (
-          <>
-            {transactions.map((transactions, index) => (
-              <Box
-                key={index}
-                alignItems="center"
-                display="flex"
-                justifyContent="space-between"
-                marginBottom={1}
-                style={{ gap: 20 }}
-              >
-                <Badge
-                  anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'right',
-                  }}
-                  badgeContent={
-                    <Avatar
-                      alt="Slowpoke"
-                      src="/fixtures/normal/slowpoke.jpg"
-                      style={{
-                        width: 20,
-                        height: 20,
-                        border: '2px solid #FFF',
-                      }}
-                    />
-                  }
-                  overlap="circle"
-                >
+  const { me } = useAuth();
+
+  const fetcher = (url: string) =>
+    tokensInstance.get(url).then((res) => res.data);
+
+  const { data: transactions } = useSWR(
+    `/api/v3/user/${me.id}/wallet/transactions`,
+    { fetcher }
+  );
+
+  const web3Config = getConfig(false); // replace by env var (mainnet / testnet)
+
+  if (!transactions) return <WalletSkeleton />;
+  // @Javier TODO show Empty if transactions.length === 0 ?
+  if (transactions && transactions.length > 0)
+    return (
+      <Box padding={2}>
+        <>
+          {transactions.map((transactions: Transaction, index) => (
+            <Box
+              key={index}
+              alignItems="center"
+              display="flex"
+              justifyContent="space-between"
+              marginBottom={1}
+              style={{ gap: 20 }}
+            >
+              <Badge // @Javier TODO show correct circle image based on transactions.type (DEPOSIT, SELL, PURCHASE...)
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+                badgeContent={
                   <Avatar
+                    alt="Slowpoke"
+                    src="/fixtures/normal/slowpoke.jpg"
                     style={{
-                      backgroundColor: index === 1 ? red[100] : green[100],
+                      width: 20,
+                      height: 20,
+                      border: '2px solid #FFF',
                     }}
-                  >
-                    <TransactionIcon
-                      style={{
-                        fill: index === 1 ? red[700] : green[700],
-                        transform: `${index === 0 && 'rotate(180deg)'}`,
-                      }}
-                    />
-                  </Avatar>
-                </Badge>
+                  />
+                }
+                overlap="circle"
+              >
+                <Avatar
+                  style={{
+                    backgroundColor: index === 1 ? red[100] : green[100],
+                  }}
+                >
+                  <TransactionIcon
+                    style={{
+                      fill: index === 1 ? red[700] : green[700],
+                      transform: `${index === 0 && 'rotate(180deg)'}`,
+                    }}
+                  />
+                </Avatar>
+              </Badge>
+              <Box display="flex" flexDirection="column">
+                <Typography
+                  style={{ lineHeight: 1.4, textTransform: 'capitalize' }}
+                  variant="button"
+                >
+                  {TransactionType[transactions.type]}
+                </Typography>
+                <Typography
+                  style={{
+                    lineHeight: 1.4,
+                    color: neutral[500],
+                    fontWeight: 400,
+                  }}
+                  variant="button"
+                >
+                  {formatTimestampToRelative(transactions.createdAt)}
+                  {`, `}
+                  {TransactionStatus[transactions.status]}
+                </Typography>
+              </Box>
+              <Box alignItems="center" display="flex" marginLeft="auto">
                 <Box display="flex" flexDirection="column">
-                  <Typography
-                    style={{ lineHeight: 1.4, textTransform: 'capitalize' }}
-                    variant="button"
-                  >
-                    {/* {transactions.info.type} */}
+                  <Typography style={{ lineHeight: 1.4 }} variant="button">
+                    {transactions.amount} SPN
                   </Typography>
                   <Typography
                     style={{
@@ -84,41 +129,27 @@ const Transactions = () => {
                     }}
                     variant="button"
                   >
-                    {/* {formatTimestampToRelative(transactions.updatedAt)} */}
-                    {`, `}
-                    {/* {transactions.info.status} */}
+                    ${transactions.amount}
                   </Typography>
                 </Box>
-                <Box alignItems="center" display="flex" marginLeft="auto">
-                  <Box display="flex" flexDirection="column">
-                    <Typography style={{ lineHeight: 1.4 }} variant="button">
-                      {/* {transactions.info.amount} SPN */}
-                    </Typography>
-                    <Typography
-                      style={{
-                        lineHeight: 1.4,
-                        color: neutral[500],
-                        fontWeight: 400,
-                      }}
-                      variant="button"
-                    >
-                      {/* ${transactions.info.amount} */}
-                    </Typography>
-                  </Box>
-                  <IconButton aria-label="go to tx">
+                <IconButton aria-label="go to tx">
+                  <Link
+                    href={`${web3Config.EXPLORER_BASE_URL}${transactions.txHash}`}
+                    rel="noreferrer"
+                    target="_blank"
+                  >
                     <ExitToAppIcon
                       fontSize="small"
                       style={{ color: neutral[400] }}
                     />
-                  </IconButton>
-                </Box>
+                  </Link>
+                </IconButton>
               </Box>
-            ))}
-          </>
-        )}
-      </Query>
-    </Box>
-  );
+            </Box>
+          ))}
+        </>
+      </Box>
+    );
 };
 
 export default Transactions;
