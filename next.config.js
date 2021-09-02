0d0d9852023a0f9d580db484a8bf6cd48a9b411d
Moises <moises@sapien.network>
2021-09-01 19:54:55 -0500

    Fix for Images on discovery page

--- next.config.js
+++ next.config.js
@@ -3,3 +3,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 });
 
 module.exports = withBundleAnalyzer({});
+
+module.exports = {
+  images: {
+    domains: ['d151dmflpumpzp.cloudfront.net'],
+  },
+};
