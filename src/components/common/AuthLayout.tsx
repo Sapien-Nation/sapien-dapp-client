bbaf414cfaaf9a24ffb603f53aa92d077bc15ebf
Sabbir <prince3339@ymail.com>
2021-09-03 01:06:35 +0600

    Auth image fit to screen viewport

--- src/components/common/AuthLayout.tsx
+++ src/components/common/AuthLayout.tsx
@@ -46,6 +46,7 @@ const AuthLayout = ({ children }: Props) => {
                 objectFit: 'cover',
                 width: '100%',
                 height: '100%',
+                position: 'absolute',
               }}
             />
           </Box>
