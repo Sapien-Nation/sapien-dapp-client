622b7df11a5f38ecd4f40e76a024bc6167658248
Ethaan <ethan.rosanoo@gmail.com>
2021-09-08 22:16:02 -0500

    Torus Network GET

--- src/api/spn-wallet/auth/initTorus.ts
+++ src/api/spn-wallet/auth/initTorus.ts
@@ -1,19 +1,28 @@
 import torusWebSdk from '@toruslabs/torus-direct-web-sdk';
 import { env, Envs, walletIsMainnet } from 'api';
 
-const initTorus = async (baseUrl: string, enableLogging = false) => {
-  // torus verifier on sandbox and local should run on testnet
-  const network = [Envs.Sandbox, Envs.Local].includes(env)
-    ? 'testnet'
-    : walletIsMainnet === 'true'
-    ? 'mainnet'
-    : 'testnet';
+enum Network {
+  Mainnet = 'mainnet',
+  Testnet = 'testnet',
+}
 
+const getNetwork = () => {
+  switch (env) {
+    case Envs.Sandbox:
+    case Envs.Local:
+      return Network.Testnet;
+      break;
+    default:
+      return walletIsMainnet === 'true' ? Network.Mainnet : Network.Testnet;
+  }
+};
+
+const initTorus = async (baseUrl: string, enableLogging = false) => {
   try {
     const torus = new torusWebSdk({
       baseUrl,
       enableLogging,
-      network,
+      network: getNetwork(), // torus verifier on sandbox and local should run on testnet
     });
     await torus.init({ skipSw: true });
     return torus;
