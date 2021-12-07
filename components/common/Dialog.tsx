79ff16c7ff7ddecdcfb5485cd4408ab1d2db3477
Moises Ocanas <moises@sapien.network>
2021-12-07 12:50:56 -0600

    feat: adding actions to dialog

--- components/common/Dialog.tsx
+++ components/common/Dialog.tsx
@@ -2,11 +2,51 @@ import { Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
 interface Props {
+  actions?: React.ReactElement;
+  cancelLabel?: string;
+  confirmLabel?: string;
   children?: React.ReactElement | null;
+  onClose?: () => void;
+  onCancel?: (data?: unknown) => void;
+  onConfirm?: (event?: unknown) => void;
+  showCancel?: boolean;
+  showConfirm?: boolean;
   title?: string;
 }
 
-const DialogElement = ({ children = null, title }: Props) => {
+const DialogElement = ({
+  cancelLabel = 'Cancel',
+  confirmLabel = 'Confirm',
+  showCancel = true,
+  showConfirm = true,
+  onClose,
+  onCancel = onClose || (() => null),
+  onConfirm,
+  actions = (
+    <>
+      {showConfirm && (
+        <button
+          type="button"
+          className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+          onClick={onConfirm}
+        >
+          {confirmLabel}
+        </button>
+      )}
+      {showCancel && (
+        <button
+          type="button"
+          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          onClick={onCancel}
+        >
+          {cancelLabel}
+        </button>
+      )}
+    </>
+  ),
+  children = null,
+  title,
+}: Props) => {
   const [open, setOpen] = useState(true);
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -52,6 +92,9 @@ const DialogElement = ({ children = null, title }: Props) => {
                 </Dialog.Title>
               </div>
               <div>{children}</div>
+              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                {actions}
+              </div>
             </div>
           </Transition.Child>
         </div>
