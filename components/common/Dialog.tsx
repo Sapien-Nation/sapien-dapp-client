8627562bd60f60b4eb46394a827363aa216ff909
Moises Ocanas <moises@sapien.network>
2021-12-08 13:07:40 -0600

    feat: dialog ui fixes

--- components/common/Dialog.tsx
+++ components/common/Dialog.tsx
@@ -1,5 +1,7 @@
 import { Fragment } from 'react';
 import { Dialog as HeadlessDialog, Transition } from '@headlessui/react';
+import { tw } from 'twind';
+import { XIcon } from '@heroicons/react/outline';
 
 interface Props {
   actions?: React.ReactElement;
@@ -29,7 +31,7 @@ const Dialog = ({
       {showConfirm && (
         <button
           type={form ? 'submit' : 'button'}
-          className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+          className={tw`w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm`}
           onClick={onConfirm}
         >
           {confirmLabel}
@@ -38,7 +40,7 @@ const Dialog = ({
       {showCancel && (
         <button
           type="button"
-          className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+          className={tw`mt-3 w-full inline-flex justify-center rounded-md border-0 px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm`}
           onClick={onCancel}
         >
           {cancelLabel}
@@ -53,10 +55,12 @@ const Dialog = ({
     <Transition.Root show as={Fragment}>
       <HeadlessDialog
         as="div"
-        className="fixed z-10 inset-0 overflow-y-auto"
+        className={tw`fixed z-10 inset-0 overflow-y-auto`}
         onClose={onClose}
       >
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div
+          className={tw`flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0`}
+        >
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -66,10 +70,12 @@ const Dialog = ({
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <HeadlessDialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <HeadlessDialog.Overlay
+              className={tw`fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity`}
+            />
           </Transition.Child>
           <span
-            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            className={tw`hidden sm:inline-block sm:align-middle sm:h-screen`}
             aria-hidden="true"
           >
             &#8203;
@@ -83,17 +89,33 @@ const Dialog = ({
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block align-bottom bg-gray-50 rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6">
+            <div
+              className={tw`inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full sm:p-6`}
+            >
+              <div
+                className={tw`hidden sm:block absolute top-0 right-0 pt-4 pr-4`}
+              >
+                <button
+                  type="button"
+                  className={tw`bg-white rounded-md text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
+                  onClick={onCancel}
+                >
+                  <span className="sr-only">Close</span>
+                  <XIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
               <div className="mt-3 sm:mt-5">
                 <HeadlessDialog.Title
                   as="h2"
-                  className="text-lg leading-6 font-medium text-gray-900"
+                  className={tw`text-xl leading-6 font-bold text-gray-900`}
                 >
                   {title}
                 </HeadlessDialog.Title>
               </div>
               <div>{children}</div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <div
+                className={tw`bg-white px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse`}
+              >
                 {actions}
               </div>
             </div>
