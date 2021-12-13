126bd4c05aaa22fcec61cdc89a7a21830dab6cc2
Moises Ocanas <moises@sapien.network>
2021-12-13 14:42:44 -0600

    feat: regex patterns for fields

--- utils/patterns.ts
+++ utils/patterns.ts
@@ -2,8 +2,9 @@
 /* eslint-disable no-useless-escape */
 export const EmailPattern =
   '^(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$';
-export const UsernamePattern = '^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$';
-export const NamePattern = "^[A-Za-z]+((s)?(('|-|.|,)?([A-Za-z])+))*$";
+export const UsernamePattern =
+  '/^(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/';
+export const NamePattern = "/^[A-Za-z]+((s)?(('|-|.|,)?([A-Za-z])+))*$/";
 export const PasswordPattern = '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$';
 
 // Tribes
