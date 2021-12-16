4241a24f97ddead2cca3cffb0a9bce91e9f5833b
javierocanas <javierocanas@gmail.com>
2021-12-16 15:25:43 -0600

    fix: Header sizing-spacing

--- components/content/Header.tsx
+++ components/content/Header.tsx
@@ -1,6 +1,6 @@
 import { tw } from 'twind';
 import {
-  ChevronLeftIcon,
+  ChevronRightIcon,
   GlobeIcon,
   UserGroupIcon,
 } from '@heroicons/react/outline';
@@ -10,7 +10,7 @@ const Header = () => {
     <div className={tw`flex items-center justify-between`}>
       <div className={tw`flex items-center gap-2 text-sm`}>
         <span
-          className={tw`inline-flex items-center justify-center h-8 w-8 rounded-full bg-gray-500`}
+          className={tw`inline-flex items-center justify-center h-8 w-8 rounded-full bg-gray-300`}
         >
           <span className={tw`text-xs font-medium leading-none text-white`}>
             TW
@@ -18,8 +18,8 @@ const Header = () => {
         </span>
         <p className={tw`text-black font-bold`}>Sapien Name</p>
         <p className={tw`text-gray-600 font-bold`}>@username</p>
-        <ChevronLeftIcon className={tw`h-6 w-6`} />
-        <GlobeIcon className={tw`h-6 w-6`} />
+        <ChevronRightIcon className={tw`h-4 w-4`} />
+        <GlobeIcon className={tw`h-4 w-4`} />
         <p className={tw`text-gray-900 font-bold`}>My Square</p>
         <span
           className={tw`bg-indigo-50 font-bold text-indigo-700 rounded-full flex gap-1 items-center px-2 py-1`}
