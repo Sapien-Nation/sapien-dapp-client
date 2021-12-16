487f0e2313159191c05fdd993a447d69c665863e
javierocanas <javierocanas@gmail.com>
2021-12-16 09:34:25 -0600

    fix: emoji popover for md devices

--- components/slate/index.tsx
+++ components/slate/index.tsx
@@ -113,14 +113,14 @@ const EditorField = () => {
                 </Popover.Button>
                 <Transition
                   as={Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="opacity-0 translate-y-1"
-                  enterTo="opacity-100 translate-y-0"
-                  leave="transition ease-in duration-150"
-                  leaveFrom="opacity-100 translate-y-0"
-                  leaveTo="opacity-0 translate-y-1"
+                  enter={tw`transition ease-out duration-200`}
+                  enterFrom={tw`opacity-0 translate-y-1`}
+                  enterTo={tw`opacity-100 translate-y-0`}
+                  leave={tw`transition ease-in duration-150`}
+                  leaveFrom={tw`opacity-100 translate-y-0`}
+                  leaveTo={tw`opacity-0 translate-y-1`}
                 >
-                  <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
+                  <Popover.Panel className="absolute z-10 right-0 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
                     <Picker onSelect={(event) => addEmoji(event)} />
                   </Popover.Panel>
                 </Transition>
