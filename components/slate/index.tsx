11a45c41a26986585364370a940189e0821b61e5
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2021-12-20 14:45:27 -0600

    feat: expanded logic for Editor

--- components/slate/index.tsx
+++ components/slate/index.tsx
@@ -16,6 +16,9 @@ import { tw } from 'twind';
 // context
 import { useAuth } from 'context/user';
 
+// components
+import { Tooltip } from 'components/common';
+
 // constants
 import { ElementType } from './constants';
 
@@ -40,10 +43,16 @@ declare module 'slate' {
   }
 }
 
+enum View {
+  Expanded,
+  Normal,
+}
+
 const createEditorWithPlugins = pipe(withReact, withHistory, useLink);
 
 const EditorField = () => {
   const editor = useMemo(() => createEditorWithPlugins(createEditor()), []);
+  const [view, setView] = useState<View>(View.Normal);
   const [value, setValue] = useState<Array<CustomElement>>([
     {
       children: [{ text: '' }],
@@ -55,7 +64,9 @@ const EditorField = () => {
   const isEditorEmpty = useIsEditorEmpty(value);
   const { renderLeaf, renderElement } = useEditorConfig(editor);
   const [previousSelection, _, setSelection] = useSelection(editor);
+
   const fileRef = useRef(null);
+  const tooltipRef = useRef(null);
 
   const { me } = useAuth();
   //--------------------------------------------------------------------------------------------------------------------
@@ -77,95 +88,109 @@ const EditorField = () => {
 
   //--------------------------------------------------------------------------------------------------------------------
   return (
-    <div
-      className={tw`flex items-center w-full mt-6 max-w-4xl rounded-2xl bg-white px-6 py-8`}
-    >
-      <img
-        className="inline-block h-10 w-10 rounded-full mr-4"
-        src={
-          me.avatar ||
-          'https://d151dmflpumpzp.cloudfront.net/tribe-images/sapien-tribe.png'
-        }
-        alt="This is your profile picture"
-        onError={(event) => {
-          (event.target as HTMLImageElement).src =
-            'https://d151dmflpumpzp.cloudfront.net/tribe-images/sapien-tribe.png';
-        }}
-      />
-      <div className={tw`w-full bg-gray-50 rounded-xl px-4 py-2 flex flex-row`}>
-        <Slate editor={editor} value={value} onChange={handleOnChange}>
-          <Editable
-            placeholder="What do you want to share?"
-            renderLeaf={renderLeaf}
-            renderElement={renderElement}
-            className={tw`w-full px-4 py-2`}
-          />
-        </Slate>
-        <div className={tw`flex justify-end items-end gap-1`}>
-          {/* Emoji Upload */}
-          <Popover className="relative">
-            {() => (
-              <>
-                <Popover.Button
-                  className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white`}
-                >
-                  <EmojiHappyIcon className={tw`h-6 w-6`} />
-                </Popover.Button>
-                <Transition
-                  as={Fragment}
-                  enter={tw`transition ease-out duration-200`}
-                  enterFrom={tw`opacity-0 translate-y-1`}
-                  enterTo={tw`opacity-100 translate-y-0`}
-                  leave={tw`transition ease-in duration-150`}
-                  leaveFrom={tw`opacity-100 translate-y-0`}
-                  leaveTo={tw`opacity-0 translate-y-1`}
-                >
-                  <Popover.Panel className="absolute z-10 right-0 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
-                    <Picker onSelect={(event) => addEmoji(event)} />
-                  </Popover.Panel>
-                </Transition>
-              </>
-            )}
-          </Popover>
-
-          {/* File Upload */}
-          <button
-            className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white`}
-            onClick={() => fileRef.current.click()}
+    <>
+      <div
+        className={tw`flex items-center w-full mt-6 max-w-4xl rounded-2xl bg-white px-6 py-8`}
+      >
+        <img
+          className="inline-block h-10 w-10 rounded-full mr-4"
+          src={
+            me.avatar ||
+            'https://d151dmflpumpzp.cloudfront.net/tribe-images/sapien-tribe.png'
+          }
+          alt="This is your profile picture"
+          onError={(event) => {
+            (event.target as HTMLImageElement).src =
+              'https://d151dmflpumpzp.cloudfront.net/tribe-images/sapien-tribe.png';
+          }}
+        />
+        {view === View.Normal && (
+          <div
+            className={tw`w-full bg-gray-50 rounded-xl px-4 py-2 flex flex-row`}
           >
-            <PhotographIcon className={tw`h-6 w-6`} />
-          </button>
-          <input
-            ref={fileRef}
-            accept="image/*"
-            className={tw`sr-only`}
-            onChange={handleAddImage}
-            type="file"
-          />
+            <Slate editor={editor} value={value} onChange={handleOnChange}>
+              <Editable
+                placeholder="What do you want to share?"
+                renderLeaf={renderLeaf}
+                renderElement={renderElement}
+                className={tw`w-full px-4 py-2`}
+              />
+            </Slate>
+            <div className={tw`flex justify-end items-end gap-1`}>
+              {/* Emoji Upload */}
+              <Popover className="relative">
+                {() => (
+                  <>
+                    <Popover.Button
+                      className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white`}
+                    >
+                      <EmojiHappyIcon className={tw`h-6 w-6`} />
+                    </Popover.Button>
+                    <Transition
+                      as={Fragment}
+                      enter={tw`transition ease-out duration-200`}
+                      enterFrom={tw`opacity-0 translate-y-1`}
+                      enterTo={tw`opacity-100 translate-y-0`}
+                      leave={tw`transition ease-in duration-150`}
+                      leaveFrom={tw`opacity-100 translate-y-0`}
+                      leaveTo={tw`opacity-0 translate-y-1`}
+                    >
+                      <Popover.Panel className="absolute z-10 right-0 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
+                        <Picker onSelect={(event) => addEmoji(event)} />
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
 
-          {/* Expand */}
+              {/* File Upload */}
+              <button
+                className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white`}
+                onClick={() => fileRef.current.click()}
+              >
+                <PhotographIcon className={tw`h-6 w-6`} />
+              </button>
+              <input
+                ref={fileRef}
+                accept="image/*"
+                className={tw`sr-only`}
+                onChange={handleAddImage}
+                type="file"
+              />
+
+              {/* Submit */}
+              <button
+                className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white`}
+                disabled={isUploadingImage}
+                onClick={handleSubmit}
+              >
+                <PaperAirplaneIcon
+                  className={tw`h-6 w-6 ${
+                    isEditorEmpty ? '' : 'rotate-90 text-indigo-500'
+                  }`}
+                />
+              </button>
+            </div>
+          </div>
+        )}
+
+        {/* Expand */}
+        {isEditorEmpty === false && view === View.Normal && (
           <button
             className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white`}
-            onClick={() => {}}
+            onClick={() => setView(View.Expanded)}
+            ref={tooltipRef.current?.setTriggerRef}
           >
             <ArrowsExpandIcon className={tw`h-6 w-6`} />
           </button>
+        )}
 
-          {/* Submit */}
-          <button
-            className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white`}
-            disabled={isUploadingImage}
-            onClick={handleSubmit}
-          >
-            <PaperAirplaneIcon
-              className={tw`h-6 w-6 ${
-                isEditorEmpty ? '' : 'rotate-90 text-indigo-500'
-              }`}
-            />
-          </button>
-        </div>
+        <Tooltip
+          ref={tooltipRef}
+          text="Use the expanded view version for better experience (BETA)"
+        />
       </div>
-    </div>
+    </>
   );
 };
 
