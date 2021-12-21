8f3e333e9764a83032a4f8cf00ff3ce9683cabc3
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2021-12-21 16:14:34 -0600

    feat: overlay for Content

--- components/slate/index.tsx
+++ components/slate/index.tsx
@@ -18,7 +18,7 @@ import { useAuth } from 'context/user';
 import { useToast } from 'context/toast';
 
 // components
-import { Tooltip } from 'components/common';
+import { Overlay, Tooltip } from 'components/common';
 
 // constants
 import { ElementType } from './constants';
@@ -199,6 +199,82 @@ const EditorField = () => {
           </form>
         )}
 
+        {view === View.Expanded && (
+          <Overlay show onClose={() => setView(View.Normal)}>
+            <form
+              className={tw`w-full bg-gray-50 rounded-xl px-4 py-2 flex flex-row`}
+              onSubmit={handleSubmit}
+            >
+              <Slate editor={editor} value={value} onChange={handleOnChange}>
+                <Editable
+                  placeholder="What do you want to share?"
+                  renderLeaf={renderLeaf}
+                  renderElement={renderElement}
+                  className={tw`w-full px-4 py-2`}
+                />
+              </Slate>
+              <div className={tw`flex justify-end items-end gap-1`}>
+                {/* Emoji Upload */}
+                <Popover className="relative">
+                  {() => (
+                    <>
+                      <Popover.Button
+                        className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white`}
+                      >
+                        <EmojiHappyIcon className={tw`h-6 w-6`} />
+                      </Popover.Button>
+                      <Transition
+                        as={Fragment}
+                        enter={tw`transition ease-out duration-200`}
+                        enterFrom={tw`opacity-0 translate-y-1`}
+                        enterTo={tw`opacity-100 translate-y-0`}
+                        leave={tw`transition ease-in duration-150`}
+                        leaveFrom={tw`opacity-100 translate-y-0`}
+                        leaveTo={tw`opacity-0 translate-y-1`}
+                      >
+                        <Popover.Panel className="absolute z-10 right-0 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-xs sm:px-0">
+                          <Picker onSelect={(event) => addEmoji(event)} />
+                        </Popover.Panel>
+                      </Transition>
+                    </>
+                  )}
+                </Popover>
+
+                {/* File Upload */}
+                <button
+                  className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white`}
+                  onClick={() => fileRef.current.click()}
+                >
+                  <PhotographIcon className={tw`h-6 w-6`} />
+                </button>
+                <input
+                  ref={fileRef}
+                  accept="image/*"
+                  className={tw`sr-only`}
+                  onChange={handleAddImage}
+                  type="file"
+                />
+
+                {/* Submit */}
+                <button
+                  className={tw`h-10 w-10 flex items-center text-gray-400 justify-center rounded-md hover:bg-gray-100 focus:bg-indigo-700 focus:text-white ${
+                    isEditorEmpty
+                      ? 'disabled:opacity-75 pointer-events-none'
+                      : 'pointer-events-auto'
+                  }`}
+                  disabled={isUploadingImage || isEditorEmpty}
+                  onClick={handleSubmit}
+                >
+                  <PaperAirplaneIcon
+                    className={tw`h-6 w-6 ${
+                      isEditorEmpty ? '' : 'rotate-90 text-indigo-500'
+                    }`}
+                  />
+                </button>
+              </div>
+            </form>
+          </Overlay>
+        )}
         {/* Expand */}
         {isEditorEmpty === false && view === View.Normal && (
           <button
