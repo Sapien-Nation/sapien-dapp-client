2e16778a076fb8a7b7b5bb47894ac7a3fe1f3dce
javierocanas <javierocanas@gmail.com>
2022-01-13 15:49:06 -0600

    feat: wallet context

---
+++ context/wallet.tsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useState, useReducer } from 'react';
+
+// reducers
+import { reducer, initialState } from 'components/wallet/WalletReducer';
+
+// types
+import type { Wallet as WalletType } from 'tools/types/wallet';
+import type { Content } from 'tools/types/content';
+
+export interface Wallet {
+  wallet: WalletType | null;
+  setWallet: (wallet: WalletType | null) => void;
+  walletOpen: Content | boolean;
+  setWalletOpen: (status: Content | boolean) => void;
+  dispatchWalletState: any;
+  globalWalletState: any;
+}
+
+export const WalletContext = createContext<Wallet>(null);
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const WalletProvider = ({ children }: Props) => {
+  const [wallet, setWallet] = useState<WalletType | null>(null);
+  const [walletOpen, setWalletOpen] = useState<Content | boolean>(false);
+  const [globalWalletState, dispatchWalletState] = useReducer(
+    reducer,
+    initialState
+  );
+  return (
+    <WalletContext.Provider
+      value={{
+        wallet,
+        setWallet,
+        walletOpen,
+        setWalletOpen,
+        dispatchWalletState,
+        globalWalletState,
+      }}
+    >
+      {children}
+    </WalletContext.Provider>
+  );
+};
+
+function useWallet() {
+  const context = useContext(WalletContext);
+
+  if (context === undefined) {
+    throw new Error('useWallet must be used within a WalletProvider');
+  }
+  return context;
+}
+
+export { WalletProvider, useWallet };
