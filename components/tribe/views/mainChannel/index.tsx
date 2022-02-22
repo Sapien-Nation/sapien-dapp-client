39e16a4afe1676b9e0b8788adbe3b71664ebfa3a
Moises Ocanas <moises@sapien.network>
2022-02-22 15:24:39 -0600

    feat: page on channel view

--- components/tribe/views/mainChannel/index.tsx
+++ components/tribe/views/mainChannel/index.tsx
@@ -1,5 +1,9 @@
 // components
-import { Head } from 'components/common';
+import { Head, Page } from 'components/common';
+import Editor from 'components/slate';
+
+// hooks
+import { useMainSquare } from 'hooks/tribe';
 
 // types
 import type { MainChannel } from 'tools/types/channel';
@@ -9,10 +13,16 @@ interface Props {
 }
 
 const MainChannelView = ({ mainChannel: { name } }: Props) => {
+  const { tribeID } = useMainSquare();
   return (
     <>
       <Head title={name} />
-      <h1>TODO use Page here</h1>
+      <Page
+        header={<h1>Header</h1>}
+        footer={<Editor tribeID={tribeID} onSave={() => {}} />}
+      >
+        <div>children</div>
+      </Page>
     </>
   );
 };
