2c038bdf70f6631cb6dea93a7fe761c54d7f095f
Moises Ocanas <moises@sapien.network>
2022-02-24 13:24:14 -0600

    feat: more height on covers

--- components/common/DefaultCover.tsx
+++ components/common/DefaultCover.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const DefaultCover = ({ name = 'Sapien' }: Props) => (
   <div
-    className={tw`bg-gradient-to-r from-purple-500 via-indigo-500 to-blue-500 h-48 relative w-full flex justify-center items-center`}
+    className={tw`bg-gradient-to-r from-purple-500 via-indigo-500 to-blue-500 h-60 relative w-full flex justify-center items-center`}
   >
     <h1 className={tw`text-xl text-white`}>{name}</h1>
   </div>
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,7 +1,7 @@
 import { Image } from '.';
 
 const Header = ({ alt, src, ...rest }) => (
-  <div style={{ position: 'relative', width: '100%', height: '200px' }}>
+  <div style={{ position: 'relative', width: '100%', height: '250px' }}>
     {/* @ts-ignore */}
     <Image {...rest} alt={alt} src={src} layout="fill" objectFit="cover" />
   </div>
