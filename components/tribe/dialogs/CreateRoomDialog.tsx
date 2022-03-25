0035bfbbf60e8edfdff1c6ff8819f04632c0c943
Moises Ocanas <moises@sapien.network>
2022-03-25 10:53:51 -0500

    feat: create room ui

--- components/tribe/dialogs/CreateRoomDialog.tsx
+++ components/tribe/dialogs/CreateRoomDialog.tsx
@@ -1,3 +1,7 @@
+import { useForm, FormProvider } from 'react-hook-form';
+import { useSWRConfig } from 'swr';
+import { useRouter } from 'next/router';
+
 // components
 import {
   Dialog,
@@ -13,11 +17,28 @@ interface Props {
   onClose: () => void;
 }
 
+interface FormValues {
+  name: string;
+}
+
 const form = 'create-room-form';
 const CreateRoomDialog = ({ onClose }: Props) => {
   const toast = useToast();
+  const methods = useForm<FormValues>({
+    defaultValues: {
+      name: '',
+    },
+  });
+  const {
+    formState: { errors, isSubmitting },
+    handleSubmit,
+    setValue,
+  } = methods;
 
-  const onSubmit = async () => {
+  const { push } = useRouter();
+  const { mutate } = useSWRConfig();
+
+  const onSubmit = async ({ name }: FormValues) => {
     try {
       onClose();
     } catch (error) {
@@ -38,9 +59,35 @@ const CreateRoomDialog = ({ onClose }: Props) => {
       <>
         <div>
           <div className="mt-5 md:mt-0 md:col-span-2">
-            <form onSubmit={onSubmit} id={form}>
-              TODO CREATE ROOM DIALOG
-            </form>
+            <FormProvider {...methods}>
+              <form onSubmit={handleSubmit(onSubmit)} id={form}>
+                <div className="flex-1">
+                  <TextInputLabel
+                    label="Name"
+                    name="name"
+                    error={errors.name?.message}
+                  />
+                  <TextInput
+                    className="block w-full bg-gray-800 pr-10 pl-3 pt-3 pb-3 border-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md"
+                    name="name"
+                    placeholder="The Sapien Tribe"
+                    maxLength={50}
+                    pattern={/^[a-zA-Z\s]$/}
+                    rules={{
+                      validate: {
+                        required: (value) => value.length > 0 || 'is required',
+                        minLength: (value) =>
+                          value?.length > 2 ||
+                          'Must be Between 2 and 50 characters long',
+                        maxLength: (value) =>
+                          value?.length <= 51 ||
+                          'Must be Between 2 and 50 characters long',
+                      },
+                    }}
+                  />
+                </div>
+              </form>
+            </FormProvider>
           </div>
         </div>
       </>
