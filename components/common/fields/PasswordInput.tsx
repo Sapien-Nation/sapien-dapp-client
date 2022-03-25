46d224d8ca1b1faa8eb25c1674d1ed0667b5d2ae
Moises Ocanas <moises@sapien.network>
2022-03-25 11:58:53 -0500

    feat: fix for input

--- components/common/fields/PasswordInput.tsx
+++ components/common/fields/PasswordInput.tsx
@@ -67,7 +67,6 @@ const PasswordInput = ({
         <input
           autoComplete={autoComplete}
           className={mergeClassNames(
-            theme && theme === 'dark' ? 'bg-gray-800' : '',
             'appearance-none block w-full px-3 py-2 border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm'
           )}
           id={name}
