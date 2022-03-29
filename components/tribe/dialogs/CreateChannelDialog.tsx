28def6ef3c1a39370eff1422775e74541657b4e9
Moises Ocanas <moises@sapien.network>
2022-03-29 15:09:51 -0500

    feat: fix for dark mode on create channel

--- components/tribe/dialogs/CreateChannelDialog.tsx
+++ components/tribe/dialogs/CreateChannelDialog.tsx
@@ -147,7 +147,7 @@ const CreateChannelDialog = ({ onClose }: Props) => {
             <FormProvider {...methods}>
               <form onSubmit={handleSubmit(onSubmit)} id={form}>
                 <div className="sm:overflow-hidden">
-                  <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+                  <div className="px-4 py-5 space-y-6 sm:p-6">
                     <div>
                       <div className="flex gap-x-4 items-end">
                         <div>
@@ -229,7 +229,7 @@ const CreateChannelDialog = ({ onClose }: Props) => {
                             error={errors.name?.message}
                           />
                           <TextInput
-                            className="block w-full pr-10 pl-3 pt-3 pb-3 bg-gray-100 border-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md"
+                            className="block w-full pr-10 pl-3 pt-3 pb-3 border-red-300 focus:outline-none focus:ring-red-500 focus:border-red-500 sm:text-sm rounded-md"
                             name="name"
                             placeholder="Channel Name"
                             maxLength={50}
@@ -250,7 +250,7 @@ const CreateChannelDialog = ({ onClose }: Props) => {
                         </div>
                       </div>
                       <div>
-                        <label className="block text-sm font-medium mt-4 text-gray-700">
+                        <label className="block text-sm font-medium mt-4">
                           Cover photo
                         </label>
                         <div className="mt-1 relative min-h-[8.75rem] flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
@@ -303,7 +303,7 @@ const CreateChannelDialog = ({ onClose }: Props) => {
                                   <div className="text-sm text-gray-600">
                                     <label
                                       htmlFor="cover-upload"
-                                      className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                                      className="relative cursor-pointer rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                                     >
                                       <span>Upload a file</span>
                                       <input
