95911768fb279e7fabd2e0bc0340691975703443
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-03-29 10:14:39 -0500

    feat: fixing 404 page test

--- __tests__/404.spec.tsx
+++ __tests__/404.spec.tsx
@@ -7,10 +7,48 @@ import NotFoundPage from 'pages/404';
 test('404 page', () => {
   render(<NotFoundPage />);
 
-  const heading = screen.getByRole('heading', {
-    name: 'Whoops looks like something went wrong...',
-  });
+  expect(screen.getByRole('img', { name: 'sapien' })).toHaveAttribute(
+    'src',
+    '/images/logooutlined.svg'
+  );
 
+  expect(
+    screen.getByRole('heading', {
+      name: 'This page does not exist.',
+    })
+  );
+  expect(
+    screen.getByRole('heading', {
+      name: 'Popular pages',
+    })
+  );
+
+  expect(
+    screen.getByRole('heading', {
+      name: '# Discovery',
+    })
+  );
+
+  expect(
+    screen.getByRole('heading', {
+      name: 'This page does not exist.',
+    })
+  );
+
+  expect(
+    (
+      screen.getByRole('link', {
+        name: '# General Room',
+      }) as HTMLLinkElement
+    ).href
+  ).toBe('http://localhost/');
+  expect(
+    (
+      screen.getByRole('link', {
+        name: '# Discovery',
+      }) as HTMLLinkElement
+    ).href
+  ).toBe('http://localhost/discovery');
   expect(
     (
       screen.getByRole('link', {
@@ -18,6 +56,17 @@ test('404 page', () => {
       }) as HTMLLinkElement
     ).href
   ).toBe('http://localhost/');
+  // const heading = screen.getByRole('heading', {
+  //   name: 'Whoops looks like something went wrong...',
+  // });
 
-  expect(heading).toBeInTheDocument();
+  // expect(
+  //   (
+  //     screen.getByRole('link', {
+  //       name: 'Back Home',
+  //     }) as HTMLLinkElement
+  //   ).href
+  // ).toBe('http://localhost//');
+
+  // expect(heading).toBeInTheDocument();
 });
