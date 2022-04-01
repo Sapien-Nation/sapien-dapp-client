aeabaaa657b387803ab4a32f71373a8fb970695b
Sabbir <prince3339@ymail.com>
2022-04-01 18:10:42 +0600

    feat: profile page added

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,14 +1,43 @@
 // components
 import { Head } from 'components/common';
 
+// context
+import { useAuth } from 'context/user';
+
 // types
 import type { NextPage } from 'next';
 
 const ProfilePage: NextPage = () => {
+  const { me } = useAuth();
+
   return (
     <>
       <Head title="Sapien" />
-      <h1>Profile</h1>
+      <div className="max-w-2xl mx-auto border-b-[1px] border-gray-800 pb-4">
+        <div className="flex items-start gap-4">
+          <div className="w-32 object-cover">
+            <img
+              className="object-cover shadow-lg rounded-xl"
+              src={
+                me.avatar ||
+                'https://cdn.discordapp.com/avatars/557967782516490270/6a43bfb06a8150801b5c3407c8103339.webp?size=240'
+              }
+              alt={me.displayName}
+            />
+          </div>
+          <div className="leading-6 font-medium space-y-1">
+            <h3 className="text-xl text-gray-400 mt-2">{me.displayName}</h3>
+            <p className="text-lg text-gray-500">{me.username}</p>
+          </div>
+        </div>
+        <div className="text-lg mt-2">
+          <p className="text-gray-500">
+            Ultricies massa malesuada viverra cras lobortis. Tempor orci hac
+            ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras.
+            Nisl dictum.
+          </p>
+        </div>
+      </div>
     </>
   );
 };
