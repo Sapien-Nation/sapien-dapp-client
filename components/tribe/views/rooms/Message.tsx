a44e236e522b957174967cc3c4a2cff956e424e4
Sabbir <prince3339@ymail.com>
2022-04-05 15:39:49 +0600

    feat: Message menu added

--- components/tribe/views/rooms/Message.tsx
+++ components/tribe/views/rooms/Message.tsx
@@ -1,3 +1,7 @@
+import { Fragment, useState } from 'react';
+import { Menu, Transition } from '@headlessui/react';
+import { DotsVerticalIcon, PencilAltIcon, TrashIcon } from '@heroicons/react/outline';
+
 // helpers
 import { formatDateRelative } from 'utils/date';
 
@@ -21,8 +25,9 @@ const Message = ({
   isAContinuosMessage,
   message: { avatarUrl, createdAt, displayName, message },
 }: Props) => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
-    <div className="py-2 hover:bg-gray-800 rounded-md px-6">
+    <div className="py-2 hover:bg-gray-800 rounded-md px-6 flex justify-between items-start group">
       <div className="flex space-x-3">
         {isAContinuosMessage && (
           <img className="h-10 w-10 rounded-full" src={avatarUrl} alt="" />
@@ -47,6 +52,51 @@ const Message = ({
           </p>
         </div>
       </div>
+      <Menu as="div" className="relative hidden group-hover:block -right-4 w-12">
+        <Menu.Button className="inline-flex absolute justify-center w-full text-sm font-medium rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+          <DotsVerticalIcon className='w-5 text-gray-400'/>
+        </Menu.Button>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute z-10 right-0 w-56 -top-3 origin-top-right bg-black divide-y divide-gray-800 rounded-md shadow-lg ring-2 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? 'bg-sapien-80 text-white' : 'text-gray-400'
+                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                  >
+                    <PencilAltIcon className='w-5 mr-2' />
+                    Edit
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+            <div className="px-1 py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? 'bg-sapien-80 text-white' : 'text-gray-400'
+                    } group flex rounded-md items-center w-full px-2 py-2 text-sm`}
+                  >
+                    <TrashIcon className='w-5 mr-2' />
+                    Delete
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
     </div>
   );
 };
