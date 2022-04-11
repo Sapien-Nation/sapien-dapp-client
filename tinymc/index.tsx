d43bdb4a3a75f667865bc30eba024527db54e390
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-04-11 12:49:35 -0500

    feat: adding editor logic

--- tinymc/index.tsx
+++ tinymc/index.tsx
@@ -1,18 +1,8 @@
 import { Editor } from '@tinymce/tinymce-react';
 
-interface Props {
-  onSubmit: () => void;
-  name: string;
-}
-const EditorComponent = ({ name }: Props) => {
+const EditorComponent = () => {
   const apiKey = process.env.NEXT_PUBLIC_TINYMC_API_KEY;
-  return (
-    <Editor
-      apiKey={apiKey}
-      cloudChannel="6-dev"
-      init={{ placeholder: `Post to #${name} ` }}
-    />
-  );
+  return <Editor apiKey={apiKey} />;
 };
 
 export default EditorComponent;
