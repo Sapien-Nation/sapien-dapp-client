5eccd772561745ef907acf2c9fb15d1661bfe544
Moises Ocanas <moises@sapien.network>
2022-04-14 11:19:01 -0500

    feat: network error view

--- components/common/views/ErrorView.tsx
+++ components/common/views/ErrorView.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import Link from 'next/link';
 
 import * as Sentry from '@sentry/nextjs';
 
@@ -12,7 +11,7 @@ interface Props {
 const ErrorView = ({
   code = 500,
   message = 'Looks like something went wrong here!',
-  onRetry,
+  onRetry = () => window.location.reload(),
 }: Props) => {
   useEffect(() => {
     const error = new Error(message);
@@ -22,24 +21,25 @@ const ErrorView = ({
   return (
     <div className="flex justify-center items-center min-h-full min-w-full">
       <div className="rounded-md p-4 text-center">
+        <img
+          className="pr-1 w-24 mx-auto pb-4"
+          src="/images/logooutlined.svg"
+          alt="sapien"
+        />
+        <h1 className="font-extrabold text-5xl mb-1 text-primary-200">
+          {code}
+        </h1>
         <h2 className="text-white text-xl font-semibold">
-          <span className="font-extrabold">{code}</span>: Sapiens.. we have a
-          problem
+          Sapiens.. we have a problem
         </h2>
-        <h2 className="text-white text-lg font-semibold">{message}</h2>
+        <h2 className="text-gray-400 text-md font-semibold mb-3">{message}</h2>
         {onRetry && (
-          <>
-            <p>
-              Seems like there is a chance to recover from this error? wanna
-              try?
-            </p>
-            <button
-              onClick={onRetry}
-              className="mt-4 inline-block py-2 px-4 border border-transparent rounded-md shadow-sm text-sm  text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
-            >
-              Retry network call
-            </button>
-          </>
+          <button
+            onClick={onRetry}
+            className="mt-4 inline-block py-2 px-4 border border-transparent rounded-md shadow-sm text-sm text-white bg-primary-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500"
+          >
+            Retry network call
+          </button>
         )}
       </div>
     </div>
