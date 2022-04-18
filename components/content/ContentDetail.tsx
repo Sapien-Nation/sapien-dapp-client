465a5946000ec8d5c2cb8ad4ada18deef2a0fc5e
Sabbir <prince3339@ymail.com>
2022-04-18 20:11:15 +0600

    feat:content detail ui updates

--- components/content/ContentDetail.tsx
+++ components/content/ContentDetail.tsx
@@ -4,6 +4,9 @@ import { ContentMimeType } from 'tools/constants/content';
 // helpers
 import { formatDateRelative } from 'utils/date';
 
+// components
+import { Avatar } from 'components/common';
+
 // types
 import type { Content as ContentType } from 'tools/types/content';
 
@@ -32,12 +35,18 @@ const ContentItem = ({
           body
         )}
         <div className="flex items-center mt-2 gap-2">
-          <div className="flex gap-1">
-            <img
-              className="w-5 h-5 object-cover rounded-full"
-              src={avatar}
-              alt="Sapien Post Image"
-            />
+          <div className="flex gap-1 items-center">
+            {avatar ? (
+              <img
+                className="w-5 h-5 object-cover rounded-full"
+                src={avatar}
+                alt="Sapien Post Image"
+              />
+            ) : (
+              <div className="bg-sapien-neutral-200 w-5 h-5 rounded-full flex-shrink-0 flex items-center justify-center">
+                {displayName[0].toUpperCase()}
+              </div>
+            )}
             <h3 className="text-sm font-medium text-gray-400">{displayName}</h3>
           </div>
           <p className="text-xs text-gray-500">
