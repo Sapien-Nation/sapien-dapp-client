289d1a3eae9a28dc98f3fce0024e4049bdb736ae
carlos <enlozanor@gmail.com>
2022-04-20 14:49:37 -0700

    feat: add animation to passport page

--- components/profile/dialog/ProfileDialogPassport.tsx
+++ components/profile/dialog/ProfileDialogPassport.tsx
@@ -2,7 +2,12 @@ import { useForm, FormProvider } from 'react-hook-form';
 import { useState, useEffect } from 'react';
 
 // components
-import { DialogPassport, TextareaInput, TextInput } from 'components/common';
+import {
+  Dialog,
+  DialogPassport,
+  TextareaInput,
+  TextInput,
+} from 'components/common';
 import { LottiePlayer } from 'lottie';
 
 // context
@@ -61,132 +66,177 @@ const ProfileDialog = ({ onClose }: Props) => {
   const renderView = () => {
     if (me.passport === null) {
       return (
-        <div className="rounded-xl mb-4 bg-transparent">
-          <div className="bg-gradient-to-r bg-transparent h-28 shadow-md rounded-lg relative flex justify-center items-center py-4" />
-          <div className="flex flex-col md:flex-row py-4 bg-transparent rounded-b-xl mb-4">
-            <div className="relative ml-4 flex flex-col items-center">
-              <div className="border border-sapien-40 w-20 h-20 -mt-16 rounded-full flex-shrink-0 bg-transparent flex items-center justify-center font-extrabold text-xl">
-                user
+        <Dialog
+          form={form}
+          show
+          isFetching={isSubmitting}
+          onClose={onClose}
+          confirmLabel="Update"
+          cancelLabel="Close"
+        >
+          <div className="rounded-xl mb-4 bg-transparent">
+            <div className="bg-gradient-to-r bg-transparent h-28 shadow-md rounded-lg relative flex justify-center items-center py-4" />
+            <div className="flex flex-col md:flex-row py-4 bg-transparent rounded-b-xl mb-4">
+              <div className="relative ml-4 flex flex-col items-center">
+                <div className="border border-sapien-40 w-20 h-20 -mt-16 rounded-full flex-shrink-0 bg-transparent flex items-center justify-center font-extrabold text-xl">
+                  user
+                </div>
+                <h1 className="text-md mt-2">{me.displayName}</h1>
               </div>
-              <h1 className="text-md mt-2">{me.displayName}</h1>
             </div>
-          </div>
-          <FormProvider {...methods}>
-            <form onSubmit={handleSubmit(onSubmit)} id={form}>
-              <div></div>
-              <TextareaInput
-                className="bg-transparent border border-sapien-40"
-                name="bio"
-                maxLength={1000}
-                placeholder="Bio"
-                rules={{
-                  validate: {
-                    maxLength: (value) => {
-                      if (value?.length > 0) {
-                        return (
-                          value?.length <= 1001 ||
-                          'Must be only 1000 characters long'
-                        );
-                      }
+            <FormProvider {...methods}>
+              <form onSubmit={handleSubmit(onSubmit)} id={form}>
+                <div></div>
+                <TextareaInput
+                  className="bg-transparent border border-sapien-40"
+                  name="bio"
+                  maxLength={1000}
+                  placeholder="Bio"
+                  rules={{
+                    validate: {
+                      maxLength: (value) => {
+                        if (value?.length > 0) {
+                          return (
+                            value?.length <= 1001 ||
+                            'Must be only 1000 characters long'
+                          );
+                        }
+                      },
                     },
-                  },
-                }}
-              />
-            </form>
-          </FormProvider>
-        </div>
+                  }}
+                />
+              </form>
+            </FormProvider>
+          </div>
+        </Dialog>
       );
     }
 
     return (
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)} id={form}>
-          <div className="flex gap-5 flex-wrap sm:flex-nowrap">
-            <div className="text-center pt-4 flex flex-col justify-between">
-              <div className="bg-sapien-60 block h-32 w-36 hexagon rotate-90 p-[1px]">
-                <div className="bg-gray-700 h-full w-full hexagon flex items-center justify-center">
-                  <img
-                    src={avatarUrl}
-                    className="-rotate-90 h-full"
-                    alt="Passport Figure generated with Machine Learning"
-                  />
+      <DialogPassport
+        form={form}
+        show
+        isFetching={isSubmitting}
+        onClose={onClose}
+        confirmLabel="Update"
+        cancelLabel="Close"
+        showCancel={false}
+        showConfirm={false}
+      >
+        <div className="px-8">
+          <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(onSubmit)} id={form}>
+              <div className="flex gap-5 flex-wrap sm:flex-nowrap">
+                <div className="text-center pt-4 flex flex-col justify-between">
+                  <div className="bg-sapien-60 block h-32 w-36 hexagon rotate-90 p-[1px]">
+                    <div className="bg-gray-700 h-full w-full hexagon flex items-center justify-center">
+                      <img
+                        src={avatarUrl}
+                        className="-rotate-90 h-full"
+                        alt="Passport Figure generated with Machine Learning"
+                      />
+                    </div>
+                  </div>
+                  <span className="hexagon-2 bg-sapien-60 p-[1px] text-sm block mt-5">
+                    <span className="hexagon-2 bg-gray-700 block text-gray-400 p-1">
+                      {me.displayName.replace(' ', '').length
+                        ? me.displayName
+                        : 'Name'}
+                    </span>
+                  </span>
                 </div>
-              </div>
-              <span className="hexagon-2 bg-sapien-60 p-[1px] text-sm block mt-5">
-                <span className="hexagon-2 bg-gray-700 block text-gray-400 p-1">
-                  {me.displayName.replace(' ', '').length
-                    ? me.displayName
-                    : 'Name'}
-                </span>
-              </span>
-            </div>
-            <div className="w-full">
-              <ul className="flex justify-between text-xs text-center">
-                <li>
-                  <span className="block text-gray-400 mb-1">Passport #</span>
-                  <span className="text-gray-300 font-semibold">
-                    {me.passport?.passportId ?? '-'}
-                  </span>
-                </li>
-                <li>
-                  <span className="block text-gray-400 mb-1">Issue Date</span>
-                  <span className="text-gray-300 font-semibold">
-                    {me.passport?.issueDate ?? '-'}
-                  </span>
-                </li>
-                <li>
-                  <span className="block text-gray-400 mb-1">
-                    Issuing Authority
-                  </span>
-                  <span className="text-gray-300 font-semibold">
-                    {me.passport?.issuingAuthority ?? '-'}
-                  </span>
-                </li>
-              </ul>
-              <div className="mt-5 flex justify-between gap-5">
-                <div
-                  style={{
-                    clipPath:
-                      'polygon(15px 0%, 100% 0%, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0% 100%, 0% 15px)',
-                  }}
-                  className="mt-3 flex-1 relative before:absolute before:pointer-events-none before:h-[35px] before:w-[1px] before:bg-sapien-60 before:rotate-[45deg] before:-top-[12px] before:left-[10px] after:absolute after:pointer-events-none after:h-[35px] after:w-[1px] after:bg-sapien-60 after:rotate-[45deg] after:-bottom-[12px] after:right-[10px]"
-                >
-                  <TextInput
-                    aria-label="name"
-                    autoComplete="name"
-                    className="!bg-transparent appearance-none min-h-[64px] border-sapien-80 block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
-                    name="name"
-                    placeholder="Name"
-                    readOnly
-                    rules={{
-                      validate: {
-                        required: (value) => value.length > 0 || 'is required',
-                      },
+                <div className="w-full">
+                  <ul className="flex justify-between text-xs text-center">
+                    <li>
+                      <span className="block text-gray-400 mb-1">
+                        Passport #
+                      </span>
+                      <span className="text-gray-300 font-semibold">
+                        {me.passport?.passportId ?? '-'}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="block text-gray-400 mb-1">
+                        Issue Date
+                      </span>
+                      <span className="text-gray-300 font-semibold">
+                        {me.passport?.issueDate ?? '-'}
+                      </span>
+                    </li>
+                    <li>
+                      <span className="block text-gray-400 mb-1">
+                        Issuing Authority
+                      </span>
+                      <span className="text-gray-300 font-semibold">
+                        {me.passport?.issuingAuthority ?? '-'}
+                      </span>
+                    </li>
+                  </ul>
+                  <div className="mt-5 flex justify-between gap-5">
+                    <div
+                      style={{
+                        clipPath:
+                          'polygon(15px 0%, 100% 0%, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0% 100%, 0% 15px)',
+                      }}
+                      className="mt-3 flex-1 relative before:absolute before:pointer-events-none before:h-[35px] before:w-[1px] before:bg-sapien-60 before:rotate-[45deg] before:-top-[12px] before:left-[10px] after:absolute after:pointer-events-none after:h-[35px] after:w-[1px] after:bg-sapien-60 after:rotate-[45deg] after:-bottom-[12px] after:right-[10px]"
+                    >
+                      <TextInput
+                        aria-label="name"
+                        autoComplete="name"
+                        className="!bg-transparent appearance-none min-h-[64px] border-sapien-80 block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
+                        name="name"
+                        placeholder="Name"
+                        readOnly
+                        rules={{
+                          validate: {
+                            required: (value) =>
+                              value.length > 0 || 'is required',
+                          },
+                        }}
+                        value={me.firstName}
+                      />
+                    </div>
+                    <div
+                      style={{
+                        clipPath:
+                          'polygon(15px 0%, 100% 0%, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0% 100%, 0% 15px)',
+                      }}
+                      className="mt-3 flex-1 relative before:absolute before:pointer-events-none before:h-[35px] before:w-[1px] before:bg-sapien-60 before:rotate-[45deg] before:-top-[12px] before:left-[10px] after:absolute after:pointer-events-none after:h-[35px] after:w-[1px] after:bg-sapien-60 after:rotate-[45deg] after:-bottom-[12px] after:right-[10px]"
+                    >
+                      <TextInput
+                        aria-label="username"
+                        autoComplete="username"
+                        className="!bg-transparent appearance-none min-h-[64px] border-sapien-80 block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
+                        name="username"
+                        placeholder="Username"
+                        readOnly
+                        rules={{
+                          validate: {
+                            required: (value) =>
+                              value.length > 0 || 'is required',
+                          },
+                        }}
+                        value={me.username}
+                      />
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      clipPath:
+                        'polygon(15px 0%, 100% 0%, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0% 100%, 0% 15px)',
                     }}
-                    value={me.firstName}
-                  />
-                </div>
-                <div
-                  style={{
-                    clipPath:
-                      'polygon(15px 0%, 100% 0%, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0% 100%, 0% 15px)',
-                  }}
-                  className="mt-3 flex-1 relative before:absolute before:pointer-events-none before:h-[35px] before:w-[1px] before:bg-sapien-60 before:rotate-[45deg] before:-top-[12px] before:left-[10px] after:absolute after:pointer-events-none after:h-[35px] after:w-[1px] after:bg-sapien-60 after:rotate-[45deg] after:-bottom-[12px] after:right-[10px]"
-                >
-                  <TextInput
-                    aria-label="username"
-                    autoComplete="username"
-                    className="!bg-transparent appearance-none min-h-[64px] border-sapien-80 block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
-                    name="username"
-                    placeholder="Username"
-                    readOnly
-                    rules={{
-                      validate: {
-                        required: (value) => value.length > 0 || 'is required',
-                      },
-                    }}
-                    value={me.username}
-                  />
+                    className="mt-3 relative before:absolute before:pointer-events-none before:h-[35px] before:w-[1px] before:bg-sapien-60 before:rotate-[45deg] before:-top-[12px] before:left-[10px] after:absolute after:pointer-events-none after:h-[35px] after:w-[1px] after:bg-sapien-60 after:rotate-[45deg] after:-bottom-[12px] after:right-[10px]"
+                  >
+                    <TextInput
+                      aria-label="title"
+                      autoComplete="title"
+                      className="!bg-transparent appearance-none min-h-[64px] border-sapien-80 block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
+                      name="title"
+                      placeholder="Title"
+                      readOnly
+                      value={me.title}
+                    />
+                  </div>
                 </div>
               </div>
               <div
@@ -196,48 +246,31 @@ const ProfileDialog = ({ onClose }: Props) => {
                 }}
                 className="mt-3 relative before:absolute before:pointer-events-none before:h-[35px] before:w-[1px] before:bg-sapien-60 before:rotate-[45deg] before:-top-[12px] before:left-[10px] after:absolute after:pointer-events-none after:h-[35px] after:w-[1px] after:bg-sapien-60 after:rotate-[45deg] after:-bottom-[12px] after:right-[10px]"
               >
-                <TextInput
-                  aria-label="title"
-                  autoComplete="title"
-                  className="!bg-transparent appearance-none min-h-[64px] border-sapien-80 block w-full px-3 py-2 border rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-purple-500 focus:border-purple-500 sm:text-sm"
-                  name="title"
-                  placeholder="Title"
+                <TextareaInput
+                  name="bio"
+                  maxLength={1000}
+                  placeholder="Bio"
+                  className="!border-[1px] border-sapien-80 !bg-transparent"
                   readOnly
-                  value={me.title}
+                  rules={{
+                    validate: {
+                      maxLength: (value) => {
+                        if (value?.length > 0) {
+                          return (
+                            value?.length <= 1001 ||
+                            'Must be only 1000 characters long'
+                          );
+                        }
+                      },
+                    },
+                  }}
+                  value={me.bio}
                 />
               </div>
-            </div>
-          </div>
-          <div
-            style={{
-              clipPath:
-                'polygon(15px 0%, 100% 0%, 100% calc(100% - 15px), calc(100% - 15px) 100%, 0% 100%, 0% 15px)',
-            }}
-            className="mt-3 relative before:absolute before:pointer-events-none before:h-[35px] before:w-[1px] before:bg-sapien-60 before:rotate-[45deg] before:-top-[12px] before:left-[10px] after:absolute after:pointer-events-none after:h-[35px] after:w-[1px] after:bg-sapien-60 after:rotate-[45deg] after:-bottom-[12px] after:right-[10px]"
-          >
-            <TextareaInput
-              name="bio"
-              maxLength={1000}
-              placeholder="Bio"
-              className="!border-[1px] border-sapien-80 !bg-transparent"
-              readOnly
-              rules={{
-                validate: {
-                  maxLength: (value) => {
-                    if (value?.length > 0) {
-                      return (
-                        value?.length <= 1001 ||
-                        'Must be only 1000 characters long'
-                      );
-                    }
-                  },
-                },
-              }}
-              value={me.bio}
-            />
-          </div>
-        </form>
-      </FormProvider>
+            </form>
+          </FormProvider>
+        </div>
+      </DialogPassport>
     );
   };
 
@@ -250,20 +283,7 @@ const ProfileDialog = ({ onClose }: Props) => {
           </div>
         </div>
       )}
-      {showDialog && (
-        <DialogPassport
-          form={form}
-          show
-          isFetching={isSubmitting}
-          onClose={onClose}
-          confirmLabel="Update"
-          cancelLabel="Close"
-          showCancel={false}
-          showConfirm={false}
-        >
-          <div className="px-8">{renderView()}</div>
-        </DialogPassport>
-      )}
+      {showDialog && renderView()}
     </>
   );
 };
