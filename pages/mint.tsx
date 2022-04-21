b827f7a75115abe11a135bea0a127c9d01770abc
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-04-21 16:25:46 -0500

    feat: Already minted page

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -17,6 +17,40 @@ import { useToast } from 'context/toast';
 import type { NextPage } from 'next';
 import type { ProfileTribe } from 'tools/types/tribe';
 
+const NothingToMintview = () => {
+  const { me } = useAuth();
+  return (
+    <div className="relative shadow-xl sm:rounded-2xl sm:overflow-hidden h-full w-full">
+      <div className="absolute inset-0">
+        <img
+          className="h-full w-full object-cover"
+          src="https://images.newindianexpress.com/uploads/user/imagelibrary/2021/11/27/w1200X800/Metaverse_is_Coming.jpg"
+          alt="People working on laptops"
+        />
+        <div className="absolute inset-0 bg-gradient-to-r from-gray-900 to-purple-900 mix-blend-multiply" />
+      </div>
+      <div className="px-4 py-4 flex flex-col gap-4 absolute justify-center items-center w-full text-center h-full">
+        <p>
+          You already have a Passport minted with the email{' '}
+          <span className="underline">{me.email}</span>
+        </p>
+        <div className="flex justify-between gap-4">
+          <Link passHref href="/logout?redirect=/mint">
+            <a className="flex justify-center h-12 items-center py-2 px-4 border-2 rounded-md shadow-sm text-md font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2">
+              Switch Account
+            </a>
+          </Link>
+          <Link passHref href="/">
+            <a className="flex justify-center h-12 items-center py-2 px-4 border-2 rounded-md shadow-sm text-md font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2">
+              Launch App
+            </a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 const distributionURL = process.env.NEXT_PUBLIC_DISTRIBUTION_URL;
 const FeedbackView = ({ code }: { code: number }) => {
   const renderText = () => {
@@ -65,7 +99,13 @@ const FeedbackView = ({ code }: { code: number }) => {
   );
 };
 
-const Mint = () => {
+const Mint = ({
+  avatar,
+  figureName,
+}: {
+  avatar: string;
+  figureName: string;
+}) => {
   const [isFetching, setIsFetching] = useState(false);
 
   const toast = useToast();
@@ -100,10 +140,13 @@ const Mint = () => {
               <h4 className="text-3xl sm:text-5xl font-extrabold bg-sapien-80">
                 Find your tribe and ignite the new renaissance!
               </h4>
-              <p>
-                Click below to mint your passport. You will be redirected to the
-                Sapien Network private beta.{' '}
-              </p>
+              <div className="aspect-w-1 aspect-h-1 sm:aspect-w-3 sm:aspect-h-2">
+                <img
+                  className="object-cover max-w-sm mx-auto rounded-xl shadow-lg shadow-white/50 border-double border-4 border-sky-500"
+                  src={avatar}
+                  alt="Sapien Avatar Transition"
+                />
+              </div>
               <button
                 type="button"
                 onClick={() => handleMint(tribes[0])}
@@ -117,10 +160,10 @@ const Mint = () => {
                 {isFetching ? (
                   <>
                     <RefreshIcon className="animate-spin h-5 w-5 mr-3" />
-                    Minting...
+                    Minting {figureName} Passport...
                   </>
                 ) : (
-                  <>Mint Passport</>
+                  <>Mint {figureName} Passport</>
                 )}
               </button>
               <p className="text-xs">
@@ -139,7 +182,9 @@ const Mint = () => {
 };
 
 interface MintStatus {
+  avatar: string | null;
   code: number | null;
+  figureName: string | null;
 }
 
 const MintPage: NextPage = () => {
@@ -156,10 +201,10 @@ const MintPage: NextPage = () => {
       </>
     );
 
-  const renderView = (code) => {
-    if (code === null) return <Redirect path="/" />;
+  const renderView = ({ avatar, code, figureName }: MintStatus) => {
+    if (code === null) return <NothingToMintview />;
 
-    if (code === 100) return <Mint />;
+    if (code === 100) return <Mint avatar={avatar} figureName={figureName} />;
 
     if (code === 101) return <FeedbackView code={101} />;
 
@@ -170,7 +215,7 @@ const MintPage: NextPage = () => {
     <>
       <SEO title="Minting" />
       <Query api="/api/v3/passport/mint-checker">
-        {({ code }: MintStatus) => renderView(code)}
+        {(response: MintStatus) => renderView(response)}
       </Query>
     </>
   );
