43ac74bfc6a436700d4c30deeefeddb2cd47cfb6
Moises Ocanas <moises@sapien.network>
2022-04-22 10:20:01 -0500

    PI-346: spinner for uploading images

--- components/tribe/dialogs/CreateTribeDialog.tsx
+++ components/tribe/dialogs/CreateTribeDialog.tsx
@@ -49,6 +49,7 @@ enum MediaTypeUpload {
 const form = 'create-tribe-form';
 const CreateTribeDialog = ({ onClose }: Props) => {
   const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [fileType, setFileType] = useState<string>('');
 
   const toast = useToast();
   const methods = useForm<FormValues>({
@@ -131,6 +132,7 @@ const CreateTribeDialog = ({ onClose }: Props) => {
     mediaTypeToUpload: MediaTypeUpload
   ) => {
     setIsUploading(true);
+    setFileType(mediaTypeToUpload);
     try {
       const formData = new FormData();
 
@@ -146,6 +148,7 @@ const CreateTribeDialog = ({ onClose }: Props) => {
         message: error,
       });
     }
+    setFileType('');
     setIsUploading(false);
   };
 
@@ -205,13 +208,38 @@ const CreateTribeDialog = ({ onClose }: Props) => {
                                 <div className="px-4 py-5 bg-gray-900 space-y-6 sm:p-6"></div>
                               </>
                             ) : (
-                              <svg
-                                className="h-full w-full text-gray-300"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
-                              >
-                                <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                              </svg>
+                              <>
+                                {isUploading &&
+                                fileType &&
+                                fileType === 'avatar' ? (
+                                  <div className="absolute top-4 right-1.5">
+                                    <svg
+                                      role="status"
+                                      className="mr-2 w-6 h-6 text-gray-200 animate-spin dark:text-gray-600 fill-primary-200"
+                                      viewBox="0 0 100 101"
+                                      fill="none"
+                                      xmlns="http://www.w3.org/2000/svg"
+                                    >
+                                      <path
+                                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                        fill="currentColor"
+                                      />
+                                      <path
+                                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                        fill="currentFill"
+                                      />
+                                    </svg>
+                                  </div>
+                                ) : (
+                                  <svg
+                                    className="h-full w-full text-gray-300"
+                                    fill="currentColor"
+                                    viewBox="0 0 24 24"
+                                  >
+                                    <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                                  </svg>
+                                )}
+                              </>
                             )}
                           </span>
                           <CameraIcon className="absolute w-5 right-0.5 bottom-0 text-gray-400" />
@@ -341,47 +369,70 @@ const CreateTribeDialog = ({ onClose }: Props) => {
                             />
                           </span>
                         ) : (
-                          <div className="space-y-1 text-center">
-                            <svg
-                              className="mx-auto h-12 w-12 text-gray-400"
-                              stroke="currentColor"
-                              fill="none"
-                              viewBox="0 0 48 48"
-                              aria-hidden="true"
-                            >
-                              <path
-                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                                strokeWidth={2}
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                              />
-                            </svg>
-                            <div className="text-sm text-gray-600">
-                              <label
-                                htmlFor="cover-upload"
-                                className="relative cursor-pointer rounded-md font-medium text-white hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                              >
-                                <span>Upload a file</span>
-                                <input
-                                  ref={coverFileInput}
-                                  id="cover-upload"
-                                  type="file"
-                                  disabled={isUploading}
-                                  aria-labelledby="Upload Cover"
-                                  className="sr-only"
-                                  onChange={(event) => {
-                                    handleUploadImage(
-                                      event.target.files[0],
-                                      MediaTypeUpload.Cover
-                                    );
-                                  }}
-                                />
-                              </label>
-                            </div>
-                            <p className="text-xs text-gray-500">
-                              PNG, JPG, GIF up to 40MB
-                            </p>
-                          </div>
+                          <>
+                            {isUploading && fileType && fileType === 'cover' ? (
+                              <div className="space-y-1 text-center self-center">
+                                <svg
+                                  role="status"
+                                  className="mr-2 w-10 h-10 text-gray-200 animate-spin dark:text-gray-600 fill-primary-200"
+                                  viewBox="0 0 100 101"
+                                  fill="none"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path
+                                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                    fill="currentColor"
+                                  />
+                                  <path
+                                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                    fill="currentFill"
+                                  />
+                                </svg>
+                              </div>
+                            ) : (
+                              <div className="space-y-1 text-center">
+                                <svg
+                                  className="mx-auto h-12 w-12 text-gray-400"
+                                  stroke="currentColor"
+                                  fill="none"
+                                  viewBox="0 0 48 48"
+                                  aria-hidden="true"
+                                >
+                                  <path
+                                    d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                                    strokeWidth={2}
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                  />
+                                </svg>
+                                <div className="text-sm text-gray-600">
+                                  <label
+                                    htmlFor="cover-upload"
+                                    className="relative cursor-pointer rounded-md font-medium text-white hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                                  >
+                                    <span>Upload a file</span>
+                                    <input
+                                      ref={coverFileInput}
+                                      id="cover-upload"
+                                      type="file"
+                                      disabled={isUploading}
+                                      aria-labelledby="Upload Cover"
+                                      className="sr-only"
+                                      onChange={(event) => {
+                                        handleUploadImage(
+                                          event.target.files[0],
+                                          MediaTypeUpload.Cover
+                                        );
+                                      }}
+                                    />
+                                  </label>
+                                </div>
+                                <p className="text-xs text-gray-500">
+                                  PNG, JPG, GIF up to 40MB
+                                </p>
+                              </div>
+                            )}
+                          </>
                         )}
                       </div>
                     </div>
