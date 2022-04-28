41e73a031edc59ba82e786119c49e9c2b5673ec4
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-04-28 11:17:49 -0500

    PVD-39: Separate API for room members

--- components/tribe/views/rooms/Details.tsx
+++ components/tribe/views/rooms/Details.tsx
@@ -6,12 +6,14 @@ import AutoSizer from 'react-virtualized-auto-sizer';
 // constats
 import { RoomMemberType } from 'tools/constants/rooms';
 
-// hooks
-import { useRoomDetails } from 'hooks/room';
+// components
+import { Query } from 'components/common';
+
+// types
+import type { RoomDetailMember } from 'tools/types/room';
 
 const Details = ({ handleSidebar }) => {
   const { query } = useRouter();
-  const { members } = useRoomDetails(query.viewID as string);
 
   const renderMemberAvatar = (avatar: string, username: string) => {
     if (avatar) {
@@ -51,46 +53,50 @@ const Details = ({ handleSidebar }) => {
           <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
         </button>
       </div>
-      <>
-        <div className="border-b border-gray-700 h-10 px-5 mb-5 w-full flex items-center">
-          <h3 className="text-md  text-sapien-neutral-400 font-bold ">
-            Members ({members.length})
-          </h3>
-        </div>
-        <ul className="px-5 overflow-auto flex-1">
-          <AutoSizer>
-            {({ height, width }) => (
-              <List
-                className="List"
-                height={height}
-                itemCount={members.length}
-                itemSize={55}
-                width={width}
-              >
-                {({ index, style }) => {
-                  const { id, avatar, username, userType } = members[index];
-                  return (
-                    <li
-                      data-testid="room-detail-member"
-                      key={id}
-                      className="flex gap-2 items-center mb-4 cursor-pointer"
-                      style={style}
-                    >
-                      {renderMemberAvatar(avatar, username)}
-                      <span className="truncate">{username}</span>
-                      {userType === RoomMemberType.Admin ? (
-                        <span className="text-xs"> (Admin) </span>
-                      ) : (
-                        ''
-                      )}
-                    </li>
-                  );
-                }}
-              </List>
-            )}
-          </AutoSizer>
-        </ul>
-      </>
+      <Query api={`/core-api/room/${query.viewID}/members`}>
+        {(members: Array<RoomDetailMember>) => (
+          <>
+            <div className="border-b border-gray-700 h-10 px-5 mb-5 w-full flex items-center">
+              <h3 className="text-md  text-sapien-neutral-400 font-bold ">
+                Members ({members.length})
+              </h3>
+            </div>
+            <ul className="px-5 overflow-auto flex-1">
+              <AutoSizer>
+                {({ height, width }) => (
+                  <List
+                    className="List"
+                    height={height}
+                    itemCount={members.length}
+                    itemSize={55}
+                    width={width}
+                  >
+                    {({ index, style }) => {
+                      const { id, avatar, username, userType } = members[index];
+                      return (
+                        <li
+                          data-testid="room-detail-member"
+                          key={id}
+                          className="flex gap-2 items-center mb-4 cursor-pointer"
+                          style={style}
+                        >
+                          {renderMemberAvatar(avatar, username)}
+                          <span className="truncate">{username}</span>
+                          {userType === RoomMemberType.Admin ? (
+                            <span className="text-xs"> (Admin) </span>
+                          ) : (
+                            ''
+                          )}
+                        </li>
+                      );
+                    }}
+                  </List>
+                )}
+              </AutoSizer>
+            </ul>
+          </>
+        )}
+      </Query>
     </aside>
   );
 };
