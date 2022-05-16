ba6544a945dd6fcb85cb22320ab9469f90323ec1
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-05-16 15:04:05 -0500

    feat: more alerts notifications

--- wallet/views/alerts/index.tsx
+++ wallet/views/alerts/index.tsx
@@ -2,6 +2,9 @@ import { ArrowLeftIcon } from '@heroicons/react/outline';
 import { useState } from 'react';
 import { useLocalStorage } from 'react-use';
 
+// components
+import { Query } from 'components/common';
+
 interface Props {
   handleBack: VoidFunction;
 }
@@ -10,8 +13,62 @@ interface Props {
 // TODO sort by date, by usin Array.sort() or just manually when adding more and more notifications
 const alerts = [
   {
-    createdAt: '2022-05-16T18:01:17.160Z', // output from new Date().toISOString();
+    createdAt: '2022-02-20T19:58:50.745Z',
     id: 1,
+    name: 'Mint Time!',
+    descriptionShort: 'Find your tribe and ignite the new renaissance!',
+    descriptionLarge: () => (
+      <div className="relative w-full h-44">
+        <div className="absolute inset-0">
+          <img
+            className="h-full w-full object-cover"
+            src="https://images.newindianexpress.com/uploads/user/imagelibrary/2021/11/27/w1200X800/Metaverse_is_Coming.jpg"
+            alt="People working on laptops"
+          />
+          <div className="absolute inset-0 bg-gradient-to-r from-gray-900 to-purple-900 mix-blend-multiply" />
+        </div>
+        <div className="relative flex justify-center items-center mb-5 flex-col h-full w-full gap-4 p-2">
+          <span>
+            The time has finally come, its time to mint your passport!
+          </span>
+          <Query api="/core-api/passport/mint-checker">
+            {({
+              code,
+              figureName,
+            }: {
+              code: number | null;
+              figureName: string;
+            }) => {
+              if (code === 100) {
+                return (
+                  <div className="mt-4 grid gap-4">
+                    <span>
+                      Please follow the instructions on your wallet to mint your
+                      Sapien Passport
+                    </span>
+                  </div>
+                );
+              }
+
+              if (code === null) {
+                <div className="mt-4 grid gap-4">
+                  <span>Find your tribe and ignite the new renaissance!</span>
+                  <span>
+                    Champion the values of {figureName} in your sapien tribe
+                  </span>
+                </div>;
+              }
+
+              return null;
+            }}
+          </Query>
+        </div>
+      </div>
+    ),
+  },
+  {
+    createdAt: '2022-05-16T18:01:17.160Z',
+    id: 2,
     name: 'Sign Passport',
     descriptionShort: 'Its time to sign your passport!',
     descriptionLarge: () => (
