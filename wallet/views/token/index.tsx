779bef4e75dd478640b505fa6dc5054c3ce656ef
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-05-16 13:58:18 -0500

    feat: sign passport shell

--- wallet/views/token/index.tsx
+++ wallet/views/token/index.tsx
@@ -1,4 +1,12 @@
-import { ArrowLeftIcon } from '@heroicons/react/outline';
+import { ArrowLeftIcon, BadgeCheckIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
+import { useSWRConfig } from 'swr';
+
+// api
+import { signPassport } from '../../api';
+
+// components
+import { Query } from 'components/common';
 
 // types
 import type { Token } from '../../types';
@@ -10,6 +18,26 @@ interface Props {
 }
 
 const TokenView = ({ handleBack, token, onWithdraw }: Props) => {
+  const [signError, setSignError] = useState<string | null>(null);
+  const [isFetching, setIsFetching] = useState(false);
+
+  const { mutate } = useSWRConfig();
+  const apiKey = `/core-api/token/${token.id}/signed`;
+
+  const handleSignToken = async () => {
+    setIsFetching(true);
+    try {
+      await signPassport(token.id);
+
+      setSignError(null);
+
+      mutate(apiKey, () => ({ signed: true }), false);
+    } catch (err) {
+      setSignError(err);
+    }
+    setIsFetching(true);
+  };
+
   return (
     <div className="bg-sapien-gray-700 overflow-hidden shadow rounded-lg w-auto h-auto py-6 px-4">
       <div className="w-72 h-96 flex flex-col  gap-4">
@@ -36,6 +64,56 @@ const TokenView = ({ handleBack, token, onWithdraw }: Props) => {
           >
             Withdraw
           </button>
+          <Query
+            api={apiKey}
+            options={{ fetcher: () => false }}
+            loader={
+              <button
+                type="button"
+                disabled
+                className="w-full animate-pulse py-2 px-4 flex justify-center items-center gap-4 border border-transparent cursor-not-allowed rounded-md shadow-sm text-sm text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+              >
+                Checking...
+              </button>
+            }
+          >
+            {({ signed }: { signed: boolean }) => {
+              if (signed === true)
+                return (
+                  <span className="text-xs text-green-400 flex justify-center items-center">
+                    Signed Passport{' '}
+                    <BadgeCheckIcon className="h-5 w-5" aria-hidden="true" />
+                  </span>
+                );
+
+              if (isFetching) {
+                return (
+                  <button
+                    type="button"
+                    disabled
+                    className="w-full py-2 px-4 flex animate-pulse justify-center items-center cursor-not-allowed gap-4 border border-transparent rounded-md shadow-sm text-sm text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                  >
+                    Signing Passport...
+                  </button>
+                );
+              }
+
+              return (
+                <button
+                  type="button"
+                  onClick={handleSignToken}
+                  className="w-full py-2 px-4 flex justify-center items-center gap-4 border border-transparent rounded-md shadow-sm text-sm text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                >
+                  Sign Passport
+                </button>
+              );
+            }}
+          </Query>
+          {signError && (
+            <span className="text-xs text-red-400 flex justify-center items-center">
+              {signError}
+            </span>
+          )}
         </div>
       </div>
     </div>
