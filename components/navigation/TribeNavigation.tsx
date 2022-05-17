6b4a6dad3201f29d87a87d1a7c4d3e8b1129c311
Sabbir <prince3339@ymail.com>
2022-05-17 17:32:14 +0600

    feat: Upgrade tribe dropdown and Create Vault item added in tribe nav

--- components/navigation/TribeNavigation.tsx
+++ components/navigation/TribeNavigation.tsx
@@ -1,7 +1,9 @@
-import { UserGroupIcon, PlusIcon } from '@heroicons/react/outline';
+import { Fragment, useEffect, useRef, useState } from 'react';
+import { SparklesIcon, PlusIcon } from '@heroicons/react/outline';
+import { Menu, Transition } from '@headlessui/react';
+import { ChevronDownIcon } from '@heroicons/react/solid';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { useState } from 'react';
 
 // constants
 import { AboutObject } from 'tools/constants/rooms';
@@ -21,7 +23,8 @@ import {
 } from 'hooks/tribe';
 import { RedDot } from 'components/common';
 
-// types
+// icons
+import VaultIcon from './assets/Vault';
 
 interface Props {
   handleMobileMenu: () => void;
@@ -72,8 +75,8 @@ const TribeNavigation = ({ handleMobileMenu }: Props) => {
               <a
                 className={
                   asPath === `/tribes/${tribeID}/home`
-                    ? 'gap-1 font-bold relative w-full cursor-pointer tracking-wide items-center uppercase font-medium text-base flex rounded-lg focus:outline-none px-4 py-2 bg-primary-200 hover:bg-sapien-neutral-800'
-                    : 'gap-1 relative w-full cursor-pointer tracking-wide items-center uppercase font-medium text-base flex rounded-lg focus:outline-none px-4 py-2 hover:bg-sapien-neutral-800'
+                    ? 'gap-1 h-10 font-bold relative w-full cursor-pointer tracking-wide items-center uppercase text-base flex rounded-lg focus:outline-none px-4 py-2 bg-primary-200 hover:bg-sapien-neutral-800'
+                    : 'gap-1 h-10 font-bold relative w-full cursor-pointer tracking-wide items-center uppercase text-base flex rounded-lg focus:outline-none px-4 py-2 hover:bg-sapien-neutral-800'
                 }
                 onClick={handleMobileMenu}
               >
@@ -82,9 +85,57 @@ const TribeNavigation = ({ handleMobileMenu }: Props) => {
                   alt="Sapien Nation"
                   className="w-6"
                 />
-                {name}
+                <span className="flex-1">{name}</span>
+                <Menu as="div" className="relative text-left -right-4">
+                  <div>
+                    <Menu.Button className="inline-flex w-full justify-center rounded-md bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+                      <ChevronDownIcon
+                        className="h-5 w-5 text-gray-50 hover:text-violet-100"
+                        aria-hidden="true"
+                      />
+                    </Menu.Button>
+                  </div>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 mt-2 w-60 origin-top-right divide-y divide-gray-100 rounded-md bg-sapien-neutral-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="px-1 py-1 ">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <button
+                              className={`${
+                                active
+                                  ? 'bg-gray-800 text-white'
+                                  : 'text-gray-300'
+                              } group flex w-full items-center rounded-sm px-2 py-2 text-sm`}
+                            >
+                              <SparklesIcon className="w-5 mr-1" />
+                              Upgrade Tribe
+                            </button>
+                          )}
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
               </a>
             </Link>
+            <button
+              aria-label="Create Room"
+              className="px-4 gap-2 py-2 mt-4 text-sm w-full flex items-center text-gray-300"
+              onClick={() => {
+                handleMobileMenu();
+              }}
+            >
+              <VaultIcon />
+              Create Vault
+            </button>
             {/* <button
               aria-label="Create Channel"
               className="px-4 py-2 mt-4 text-xs w-full flex justify-between items-center text-sapien-neutral-200 font-bold"
