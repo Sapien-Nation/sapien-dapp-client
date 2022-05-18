fcf61b48600f0e420ebd18fa45eb1c841487cb88
Sabbir <prince3339@ymail.com>
2022-05-19 00:17:31 +0600

    feat: message loading bg color updates

--- components/tribe/views/rooms/views/Skeleton.tsx
+++ components/tribe/views/rooms/views/Skeleton.tsx
@@ -11,7 +11,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-full">
             <div className="bg-sapien-neutral-200 w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-full bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-full bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -20,7 +20,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-72">
             <div className=" w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-72 bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-72 bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -29,7 +29,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-full">
             <div className=" w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-full bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-full bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -38,7 +38,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-36">
             <div className="bg-sapien-neutral-200 w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-36 bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-36 bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -53,7 +53,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-full">
             <div className="bg-sapien-neutral-200 w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-full bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-full bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -62,7 +62,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-28">
             <div className=" w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-28 bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-28 bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -71,7 +71,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-32">
             <div className=" w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-32 h-26 bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-32 h-26 bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -80,7 +80,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-96">
             <div className=" w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-96 h-36 bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-96 h-36 bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -95,7 +95,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-full">
             <div className="bg-sapien-neutral-200 w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-full bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-full bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -104,7 +104,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-full">
             <div className=" w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-full h-56 bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-full h-56 bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -119,7 +119,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-96">
             <div className="bg-sapien-neutral-200 w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-96 bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-96 bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
@@ -128,7 +128,7 @@ const Skeleton = () => {
         <div className="py-2 rounded-md px-6 flex justify-between items-start group">
           <div className="flex space-x-3 w-56">
             <div className=" w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center" />
-            <div className="flex-1 space-y-1 w-56 bg-gray-300 rounded-md"></div>
+            <div className="flex-1 space-y-1 w-56 bg-gray-700 rounded-md"></div>
           </div>
         </div>
       </li>
