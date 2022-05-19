8970891f60a2211e775557978eaa34e712e6d110
Sabbir <prince3339@ymail.com>
2022-05-19 22:05:10 +0600

    feat: Notification overflow issue fix

--- components/navigation/TribeNavigation.tsx
+++ components/navigation/TribeNavigation.tsx
@@ -167,7 +167,7 @@ const TribeNavigation = ({ handleMobileMenu }: Props) => {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                    <Menu.Items className="absolute right-0 mt-2 w-60 origin-top-right divide-y divide-gray-700 rounded-md bg-sapien-neutral-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <Menu.Items className="absolute z-10 right-0 mt-2 w-60 origin-top-right divide-y divide-gray-700 rounded-md bg-sapien-neutral-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       {isTribeOwnerOrTribeAdmin && (
                         <div className="px-1 py-1">
                           {tribe.isUpgraded === true && (
