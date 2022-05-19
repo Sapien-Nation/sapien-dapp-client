43d4095bbb97ef73558c9c6d40360a124ebc756b
Sabbir <prince3339@ymail.com>
2022-05-19 19:14:22 +0600

    feat: Tribe upgrade Wallet Token Page UI

--- components/tribe/views/upgrade/views/wallet/index.tsx
+++ components/tribe/views/upgrade/views/wallet/index.tsx
@@ -305,14 +305,14 @@ const Wallet = () => {
         return <UpgradeView />;
       case View.DeclarationOfSovereignty:
         return (
-          <div className="overflow-hidden shadow rounded-lg divide-y divide-gray-200">
-            <div className="px-4 py-5 sm:px-6">
-              <h5 className="text-xl text-white font-bold tracking-wide text-center">
+          <div>
+            <div className="px-4 py-5 sm:px-6 flex flex-col items-center gap-3">
+              <h1 className="text-xl lg:text-3xl italic text-white font-bold tracking-wide text-center underline decoration-double decoration-gray-500 decoration-2">
                 Declaration of Sovereignty of The Sapiens
-              </h5>
+              </h1>
             </div>
-            <div className="px-4 py-5 sm:p-6">
-              <div className="text-justify px-3">
+            <div>
+              <div className="text-justify text-gray-300">
                 <p>
                   The world is at a turning point and we are faced with
                   tremendous uncertainty in all directions. As the threats we
@@ -433,18 +433,18 @@ const Wallet = () => {
                 </ol>
               </div>
             </div>
-
-            <div className="py-4 flex gap-4">
+            <div className="mb-4 mt-6 flex gap-10 justify-center">
               <button
+                type="button"
                 onClick={() => setView(View.Tokens)}
-                className="w-full py-2 px-4 flex justify-center items-center gap-4 border border-transparent rounded-md shadow-sm text-sm text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                className="py-2 px-4 flex-1 justify-center items-center gap-4 border border-transparent rounded-md shadow-sm text-sm text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
               >
                 Back
               </button>
               <button
                 type="button"
                 onClick={() => setView(View.ConfirmSign)}
-                className="w-full py-2 px-4 mt-4 flex justify-center items-center gap-4 border border-transparent rounded-md shadow-sm text-sm text-white bg-black hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
+                className="py-2 px-4 flex-1 justify-center items-center gap-4 border border-transparent rounded-md shadow-sm text-sm text-white bg-primary hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black"
               >
                 Continue
               </button>
