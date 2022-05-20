df013553239db0bdb5d18277c857a85a5638aea9
Sabbir <prince3339@ymail.com>
2022-05-20 17:25:03 +0600

    feat: Badges Welcome page ui

--- components/tribe/views/badges/index.tsx
+++ components/tribe/views/badges/index.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import { XIcon } from '@heroicons/react/outline';
 
 // constants
 import { Role } from 'tools/constants/tribe';
@@ -37,8 +38,37 @@ const BadgesView = () => {
       case View.Home: {
         return (
           <div>
-            Tribe Badges Home View nice to have an explanation and a welcome
-            screen
+            <div className="bg-gradient-to-r to-[#6200ea] from-black px-4 py-8 font-semibold text-lg lg:text-2xl mt-3 rounded-md">
+              <h1>Unlock your power with Badges!</h1>
+            </div>
+            <div className="whitespace-pre-line text-gray-300 space-y-2 pt-5 text-justify">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+                Veritatis obcaecati tenetur iure eius earum ut molestias
+                architecto voluptate aliquam nihil, eveniet aliquid culpa
+                officia aut! Impedit sit sunt quaerat, odit, tenetur error,
+                harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+              </p>
+              <p>
+                quia. Quo neque error repudiandae fuga? Ipsa laudantium
+                molestias eos sapiente officiis modi at sunt excepturi expedita
+                sint?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Maxime mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam blanditiis harum
+                quisquam eius sed odit fugiat iusto fuga praesentium optio,
+                eaque rerum! Provident similique accusantium nemo autem.
+                Veritatis obcaecati tenetur iure eius earum ut molestias
+                architecto voluptate aliquam nihil, eveniet aliquid culpa
+                officia aut! Impedit sit sunt quaerat, odit, tenetur error,
+                harum nesciunt ipsum debitis quas aliquid. Reprehenderit, quia.
+                Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
+                sapiente officiis modi at sunt excepturi expedita sint?
+              </p>
+            </div>
           </div>
         );
       }
@@ -56,54 +86,27 @@ const BadgesView = () => {
 
   return (
     <Overlay onClose={back} isOpen={isOpen}>
-      <div className="hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0">
-        <Sidebar
-          setSelectedBadge={(badge) => {
-            setSelectedBadge(badge);
-            setView(View.BadgeCreation);
-          }}
-          showSearch={() => setView(View.Search)}
-        />
-      </div>
-      <div className="md:pl-64 flex flex-col flex-1 text-black">
-        <div className="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100">
-          <button
-            type="button"
-            className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-            onClick={() => null}
-          >
-            <span className="sr-only">Close Badges Managment</span>X
-          </button>
+      <div className="flex h-full">
+        <div className="hidden md:flex md:w-64 md:flex-col md:inset-y-0">
+          <Sidebar
+            setSelectedBadge={(badge) => {
+              setSelectedBadge(badge);
+              setView(View.BadgeCreation);
+            }}
+            showSearch={() => setView(View.Search)}
+          />
         </div>
-        <div className="flex-1">
-          <div className="py-6">
-            <div className="flex justify-between max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <button
-                type="button"
-                onClick={() => setIsOpen(false)}
-                className="rounded-md bg-white text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-              >
-                <span className="sr-only">Close pannel</span>
-                <svg
-                  className="h-6 w-6"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="2"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              <div className="py-4">{renderView()}</div>
-            </div>
+        <div className="flex-1 overflow-auto">
+          <div className="max-w-2xl mx-auto px-4 sm:px-6 md:px-8 relative">
+            <button
+              type="button"
+              className="rounded-md text-gray-400 hover:text-gray-500 focus:outline-none absolute right-0 top-5"
+              onClick={() => setIsOpen(false)}
+            >
+              <span className="sr-only">Close Badges Managment</span>
+              <XIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+            <div className="py-8">{renderView()}</div>
           </div>
         </div>
       </div>
