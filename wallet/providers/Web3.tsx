3ad90e29c9c46ef691baba875c708de85f67cd4a
mateo <mateo@sapien.network>
2022-05-31 03:00:22 -0700

    update withdraw error handle

--- wallet/providers/Web3.tsx
+++ wallet/providers/Web3.tsx
@@ -31,6 +31,7 @@ import { hooks as metaMaskHooks } from '../connectors/metaMask';
 import type { Token } from '../types';
 import type { AbiItem } from 'web3-utils';
 import type { Transaction } from 'tools/types/web3';
+import { TransactionReceipt } from 'web3-core';
 
 // web3
 import Web3Library from 'web3';
@@ -54,6 +55,11 @@ interface Web3 {
 
 interface Web3ProviderProps {
   children: React.ReactNode;
+}
+
+interface TxResult {
+  data: TransactionReceipt;
+  type: ErrorTypes;
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -279,24 +285,30 @@ const Web3Provider = ({ children }: Web3ProviderProps) => {
         );
 
         // Get the transaction Hash using the Event Emitter returned
-        const data = await WalletAPIRef.current.eth.sendSignedTransaction({
-          signature: signature,
-          forwardRequest: forwardData.request,
-          rawTransaction: signedTx.rawTransaction,
-          signatureType: biconomy.EIP712_SIGN,
+        const result: TxResult = await new Promise(async (resolve, reject) => {
+          WalletAPIRef.current.eth
+            .sendSignedTransaction({
+              signature: signature,
+              forwardRequest: forwardData.request,
+              rawTransaction: signedTx.rawTransaction,
+              signatureType: biconomy.EIP712_SIGN,
+            })
+            .on('transactionHash', (hash: string) =>
+              console.log('Transaction hash: ', hash)
+            )
+            .on('receipt', (rec: TransactionReceipt) =>
+              resolve({ data: rec, type: ErrorTypes.Success })
+            )
+            .on('error', (err) => {
+              // If the transaction was rejected by the network with a receipt, the receipt will be available as a property on the error object.
+              if (err.receipt)
+                resolve({ data: err.receipt, type: ErrorTypes.Fail });
+              reject(err);
+            });
         });
 
-        if (data === null) {
-          // @see https://web3js.readthedocs.io/en/v1.2.11/web3-eth.html#eth-gettransactionreceipt-return 'null if no receipt was found:'
-          return Promise.reject('Receipt Address was found.');
-        }
-
-        if (data.error) {
-          return { hash: data.transactionHash, type: ErrorTypes.Fail };
-        }
-
         await withdraw(tokenId);
-        return { hash: data.transactionHash, type: ErrorTypes.Success };
+        return { hash: result.data.transactionHash, type: result.type };
       }
       return Promise.reject('Token does not belong to this wallet.');
     } catch (err) {
