c1f5c4a872e66bc73e3f2ef587d065fb44908942
Sabbir <prince3339@ymail.com>
2022-06-14 21:58:44 +0600

    feat: manage members ui updates

--- components/tribe/views/badges/badge/manage/views/Members.tsx
+++ components/tribe/views/badges/badge/manage/views/Members.tsx
@@ -204,19 +204,19 @@ const MembersView = ({ badge }: Props) => {
       );
     }
 
-    if (transaction.signedBy.includes(me.id)) return <span>Signed</span>;
+    if (transaction.signedBy?.includes(me.id)) return <span>Signed</span>;
 
-    if (transaction.rejectedBy.includes(me.id)) return <span>Rejected</span>;
+    if (transaction.rejectedBy?.includes(me.id)) return <span>Rejected</span>;
 
     return (
-      <div>
+      <div className="flex gap-10">
         <button
           onClick={() => handleRejectTransaction(transaction.safeTxHash)}
           type="button"
           className={
             isRejecting
-              ? 'cursor-not-allowed w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-sapien hover:bg-sapien-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500'
-              : 'w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-sapien hover:bg-sapien-80 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500'
+              ? 'cursor-not-allowed w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-semibold text-white bg-sapien-red-700 over:bg-sapien-red-700/50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500'
+              : 'w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-semibold text-white bg-sapien-red-700 hover:bg-sapien-red-700/50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500'
           }
           disabled={isRejecting}
         >
@@ -244,33 +244,39 @@ const MembersView = ({ badge }: Props) => {
     <div className="flex flex-col gap-9 px-3 py-4">
       {badgeTransactions.length > 0 ? (
         <div>
-          <h1>Transactions</h1>
+          <h2 className="text-lg text-gray-300">Transactions</h2>
           <ol>
             {badgeTransactions.map((transaction) => (
               <li key={transaction.id}>
-                <div className="flex">
-                  <span>
+                <div className="flex gap-3 items-center mt-5">
+                  <span className="text-gray-400 font-semibold">
                     {transaction.approvals} of {transaction.requiredApprovals}{' '}
                     Required Approvals
                   </span>
                   {/* TODO something like this https://stackoverflow.com/questions/48916431/overlapping-overlaying-multiple-inline-images */}
-                  {transaction.grantToUsers.map((transactionUser) => (
-                    <div key={transactionUser.id}>
-                      {transactionUser.avatar ? (
-                        <img
-                          className="w-10 h-10"
-                          src={transactionUser.avatar}
-                          alt=""
-                        />
-                      ) : (
-                        <div className="bg-sapien-neutral-200 w-10 h-10 rounded-full flex-shrink-0 flex items-center justify-center">
-                          {transactionUser.username[0].toUpperCase()}
+                  {transaction.grantToUsers?.length > 0 ? (
+                    <div className="flex -space-x-2">
+                      {transaction.grantToUsers.map((transactionUser) => (
+                        <div key={transactionUser.id} className="inline-block">
+                          {transactionUser.avatar ? (
+                            <img
+                              className="h-8 w-8 rounded-full ring-2 ring-white"
+                              src={transactionUser.avatar}
+                              alt=""
+                            />
+                          ) : (
+                            <div className="bg-sapien-neutral-200 h-8 w-8 ring-2 ring-white rounded-full flex-shrink-0 flex items-center justify-center">
+                              {transactionUser.username[0].toUpperCase()}
+                            </div>
+                          )}
                         </div>
-                      )}
+                      ))}
                     </div>
-                  ))}
+                  ) : null}
                 </div>
-                {renderTransactionActions(transaction)}
+                <div className="mt-5">
+                  {renderTransactionActions(transaction)}
+                </div>
               </li>
             ))}
           </ol>
@@ -279,13 +285,16 @@ const MembersView = ({ badge }: Props) => {
 
       {badge.owners.length > 0 ? (
         <div>
-          <h1>Current Badge Owners</h1>
-
-          <ol>
+          <h2 className="text-gray-400 font-semibold">Current Badge Owners</h2>
+          <ol className="space-y-4 mt-4">
             {badge.owners.map((owner) => {
               return (
-                <li key={owner.id}>
-                  <img src={owner.avatar} alt="" />
+                <li key={owner.id} className="flex gap-3 items-center">
+                  <img
+                    className="h-8 w-8 rounded-full ring-2 ring-white"
+                    src={owner.avatar}
+                    alt=""
+                  />
                   <span>{owner.username}</span>
                 </li>
               );
@@ -295,9 +304,11 @@ const MembersView = ({ badge }: Props) => {
       ) : null}
 
       {availableOwnersToPropose.length > 0 ? (
-        <>
-          <h1>Propose new badge owners</h1>
-          <div className="flex flex-col items-center relative">
+        <div>
+          <h2 className="text-gray-400 font-semibold">
+            Propose new badge owners
+          </h2>
+          <div className="flex flex-col items-center relative mt-4">
             <div className="w-full">
               <div className="p-1 flex border border-sapien-neutral-400 bg-sapien-neutral-500 placeholder-sapien-neutral-200 rounded">
                 <div className="flex flex-auto flex-wrap">
@@ -394,7 +405,7 @@ const MembersView = ({ badge }: Props) => {
               </div>
             </div>
           </div>
-          <div>
+          <div className="mt-4">
             <button
               onClick={handleProposeTransaction}
               type="button"
@@ -411,17 +422,17 @@ const MembersView = ({ badge }: Props) => {
               {isProposing ? 'Submitting...' : `Propose Transaction`}
             </button>
             {newOwners.length === 0 ? (
-              <span>
+              <span className="mt-1 block text-sm text-gray-400">
                 Select at least 1 tribe member to propose a new transaction
               </span>
             ) : (
-              <span>
+              <span className="mt-1 block text-sm text-gray-400">
                 {newOwners.length} will be proposed to receive the {badge.name}{' '}
                 badge
               </span>
             )}
           </div>
-        </>
+        </div>
       ) : null}
     </div>
   );
