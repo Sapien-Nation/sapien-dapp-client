d0dccc031fff6494cfab16b3afa0ba61a9463574
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-07-13 10:39:10 -0500

    feat: fixing notifications

--- next.config.js
+++ next.config.js
@@ -75,10 +75,6 @@ const moduleExports = {
           destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v3/:path*`,
         },
         {
-          source: '/core-api/notification/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/v3/notification/:path*`,
-        },
-        {
           source: '/core-api/user/:path*',
           destination: `${process.env.NEXT_PUBLIC_API_AUTH_URL}/api/v3/:path*`,
         },
