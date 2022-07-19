6e6fc6276f8c9468e6998ec4376bc039f0e1a172
carlos <enlozanor@gmail.com>
2022-07-19 10:28:56 -0700

    feat: channel header UI improvements

--- components/tribe/views/channel/ChannelHeader.tsx
+++ components/tribe/views/channel/ChannelHeader.tsx
@@ -1,4 +1,9 @@
-import { DocumentDuplicateIcon } from '@heroicons/react/outline';
+import {
+  DocumentDuplicateIcon,
+  DotsHorizontalIcon,
+  PencilIcon,
+} from '@heroicons/react/outline';
+import { Menu, Transition } from '@headlessui/react';
 
 import { useRouter } from 'next/router';
 import { useCopyToClipboard } from 'react-use';
@@ -20,6 +25,9 @@ interface Props {
 const ChannelHeader = ({ channel, handleWriteAnArticle }: Props) => {
   const toast = useToast();
   const { query } = useRouter();
+  // TODO: Check permissions
+  const canEdit = true;
+  const canDelete = true;
   const [_, copyToClipboard] = useCopyToClipboard();
 
   const { tribeID } = query;
@@ -45,61 +53,80 @@ const ChannelHeader = ({ channel, handleWriteAnArticle }: Props) => {
       ) : (
         <div className="bg-gradient-to-r bg-sapien-neutral-200 min-h-250 shadow-md rounded-lg relative flex justify-center items-center" />
       )}
-      <div className="flex flex-col md:flex-row">
+      <div className="flex flex-col md:flex-row sm:mx-8">
         <div className="relative">
           {channel.avatar ? (
             <img
-              className="w-40 h-40 mr-auto ml-auto sm:mr-0 sm:ml-8 -mt-16 rounded-xl flex-shrink-0 object-cover"
+              className="w-40 h-40 mr-auto ml-auto md:mr-0 -mt-16 rounded-xl flex-shrink-0 object-cover"
               src={channel.avatar}
               alt=""
             />
           ) : (
-            <div className="w-40 h-40 mr-auto ml-auto sm:mr-0 sm:ml-8 -mt-16 rounded-xl flex-shrink-0 bg-sapien-neutral-200 shadow shadow-sapien-neutral-600 flex items-center justify-center font-bold text-xl">
+            <div className="w-40 h-40 mr-auto ml-auto md:mr-0 -mt-16 rounded-xl flex-shrink-0 bg-sapien-neutral-200 shadow shadow-sapien-neutral-600 flex items-center justify-center font-bold text-xl">
               {channel.name[0].toUpperCase()}
             </div>
           )}
         </div>
-        <div className="flex flex-col sm:flex-row justify-center mt-3 items-center sm:justify-between w-full">
-          <div className="flex flex-col justify-center sm:ml-12">
+        <div className="flex flex-col md:flex-row justify-center mt-3 items-center sm:justify-between w-full">
+          <div className="flex flex-col justify-center items-center md:ml-7">
             <h1 className="text-xl text-center sm:text-left font-semibold">
               {channel.name}
             </h1>
-            <div className="flex">
-              <h2 className="text-gray-500 mb-4 sm:mb-0">
-                {channel.membersCount} members
-              </h2>
-            </div>
+
+            <h2 className="text-gray-500 mb-4 sm:mb-0">
+              {channel.membersCount} members
+            </h2>
           </div>
-          <div className="flex items-center sm:mr-5">
-            {/* <button
-              onClick={handleCopyToClipboard}
-              type="button"
-              className="relative inline-flex items-center px-4 py-2 rounded-l-md border-0 bg-primary-200 focus:outline-none font-semibold"
-            >
-              Invite
-            </button>
-            <button
-              onClick={handleCopyToClipboard}
-              type="button"
-              className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border-l bg-primary-200 font-medium focus:outline-none mr-3"
-            >
-              <DocumentDuplicateIcon className="w-6" />
-            </button> */}
-            <button
-              onClick={handleWriteAnArticle}
-              type="button"
-              className="relative inline-flex items-center px-4 py-2 rounded-md border-0 bg-primary-200 font-medium focus:outline-none"
-            >
-              Write an Article
-            </button>
+          <div className="flex items-center gap-2">
+            <div className="flex items-center">
+              <button
+                onClick={handleCopyToClipboard}
+                type="button"
+                className="relative inline-flex items-center px-4 py-2 rounded-l-md border-0 bg-primary-200 focus:outline-none font-semibold"
+              >
+                Invite
+              </button>
+              <button
+                onClick={handleCopyToClipboard}
+                type="button"
+                className="-ml-px relative inline-flex items-center px-4 py-2 rounded-r-md border-l bg-primary-200 font-medium focus:outline-none"
+              >
+                <DocumentDuplicateIcon className="w-6" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-      {channel.description && (
-        <p className="ml-8 mt-8 text-gray-300 whitespace-pre-line line-clamp-5">
+      <div className="flex flex-col md:flex-row justify-between mt-8 mx-0 md:mx-8">
+        <p className="text-gray-300 whitespace-pre-line line-clamp-5 flex-1">
           {channel.description}
         </p>
-      )}
+        <div className="flex justify-end items-center gap-4 text-xs text-gray-400">
+          <button
+            className={`${canEdit ? 'visible' : 'hidden'} `}
+            onClick={() => null}
+          >
+            <PencilIcon className="w-5 " aria-hidden="true" />
+          </button>
+          <Menu as="div" className={`${canDelete ? 'visible' : 'hidden'} `}>
+            <Menu.Button>
+              <DotsHorizontalIcon className="w-5" />
+            </Menu.Button>
+            <Transition>
+              <Menu.Items className="absolute w-56 origin-top-right bg-sapien-neutral-900 rounded-md p-2 hover:bg-gray-800 z-10">
+                <Menu.Item>
+                  <button
+                    className="w-full text-left text-sm text-white"
+                    onClick={() => null}
+                  >
+                    Delete Channel
+                  </button>
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
+      </div>
     </div>
   );
 };
