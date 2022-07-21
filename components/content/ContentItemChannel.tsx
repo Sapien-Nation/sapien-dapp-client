4f47153245657ab83f4430bd85d085bec645ecc4
carlos <enlozanor@gmail.com>
2022-07-21 11:37:18 -0700

    feat: add breadcrumbs

--- components/content/ContentItemChannel.tsx
+++ components/content/ContentItemChannel.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { GlobeIcon, UserGroupIcon } from '@heroicons/react/solid';
 // TODO: commented until we have data to show
 // import {
@@ -54,56 +55,69 @@ const ContentItem = ({
   tribeID,
 }: Props) => {
   const tribe = useTribe(tribeID);
+  const { query } = useRouter();
+
+  const showBreadcrumbs = query.id;
 
   return (
-    <div className="flex flex-col bg-sapien-neutral-600 rounded-xl p-3 gap-1">
-      <div className="flex items-center justify-between p-3 flex-wrap">
-        <div className="flex items-center gap-3">
-          {avatar ? (
-            <img
-              className="w-10 h-10 object-cover rounded-full"
-              src={avatar}
-              alt="Sapien Post Image"
-            />
-          ) : (
-            <div className="w-5 h-5 bg-sapien-neutral-200 rounded-full flex items-center justify-center font-bold text-sm">
-              {username[0].toUpperCase()}
-            </div>
-          )}
-          <h3 className="text-sm font-semibold text-white">{displayName}</h3>
-          <h3 className="text-sm text-gray-500">@{username}</h3>
-          <RightTriangle />
-          <p className="font-bold flex items-center gap-1">
-            <GlobeIcon className="h-4 text-gray-500" />
-            {group.name}
-          </p>
-          <p className="text-sm text-sapien-40 font-semibold bg-sapien-80/40 rounded-2xl py-1 px-2 flex gap-1">
-            <UserGroupIcon className="h-4" />
-            {tribe.name}
+    <>
+      {showBreadcrumbs && (
+        <div className="flex items-center gap-1">
+          <span className="text-sm font-semibold text-white">{group.name}</span>
+          <span className="text-gray-500">/</span>
+          <span className="text-sm text-gray-500">PostView</span>
+        </div>
+      )}
+      <div className="flex flex-col bg-sapien-neutral-600 rounded-xl p-3 gap-1">
+        <div className="flex items-center justify-between p-3 flex-wrap">
+          <div className="flex items-center gap-3">
+            {avatar ? (
+              <img
+                className="w-10 h-10 object-cover rounded-full"
+                src={avatar}
+                alt="Sapien Post Image"
+              />
+            ) : (
+              <div className="w-5 h-5 bg-sapien-neutral-200 rounded-full flex items-center justify-center font-bold text-sm">
+                {username[0].toUpperCase()}
+              </div>
+            )}
+            <h3 className="text-sm font-semibold text-white">{displayName}</h3>
+            <h3 className="text-sm text-gray-500">@{username}</h3>
+            <RightTriangle />
+            <p className="font-bold flex items-center gap-1">
+              <GlobeIcon className="h-4 text-gray-500" />
+              {group.name}
+            </p>
+            <p className="text-sm text-sapien-40 font-semibold bg-sapien-80/40 rounded-2xl py-1 px-2 flex gap-1">
+              <UserGroupIcon className="h-4" />
+              {tribe.name}
+            </p>
+          </div>
+          <p className="text-sm text-gray-500">
+            {formatDateRelative(createdAt)}
           </p>
         </div>
-        <p className="text-sm text-gray-500">{formatDateRelative(createdAt)}</p>
-      </div>
-      <div className="flex-1 p-3">
-        {mimeType === ContentMimeType.Html ? (
-          <div
-            dangerouslySetInnerHTML={{
-              __html: body,
-            }}
-          ></div>
-        ) : (
-          body
+        <div className="flex-1 p-3">
+          {mimeType === ContentMimeType.Html ? (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: body,
+              }}
+            ></div>
+          ) : (
+            body
+          )}
+        </div>
+        {imagePreview && (
+          <img
+            className="object-cover rounded-md"
+            src={imagePreview}
+            alt="Sapien Post Image"
+          />
         )}
-      </div>
-      {imagePreview && (
-        <img
-          className="object-cover rounded-md"
-          src={imagePreview}
-          alt="Sapien Post Image"
-        />
-      )}
-      {/* TODO: commented until we have data to show */}
-      {/* <div className="flex items-center gap-5 text-gray-500 p-3">
+        {/* TODO: commented until we have data to show */}
+        {/* <div className="flex items-center gap-5 text-gray-500 p-3">
         <span className="flex items-center gap-2">
           <ChatIcon className="h-4" />
         </span>
@@ -114,7 +128,8 @@ const ContentItem = ({
           <ShareIcon className="h-4" />
         </span>
       </div> */}
-    </div>
+      </div>
+    </>
   );
 };
 
