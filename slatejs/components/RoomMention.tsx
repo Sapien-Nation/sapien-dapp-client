363de9bbb6c3ece72e77119c90f249f3b61c8cd6
carlos <enlozanor@gmail.com>
2022-08-08 11:16:17 -0700

    feat: room/user mention tag styling

--- slatejs/components/RoomMention.tsx
+++ slatejs/components/RoomMention.tsx
@@ -1,23 +1,21 @@
 import { useSelected, useFocused } from 'slate-react';
 
+// helpers
+import { TAG_CLASS } from 'components/tribe/views/rooms/helpers';
+
 const RoomMention = ({ attributes, children, element }) => {
   const focused = useFocused();
   const selected = useSelected();
 
   return (
-    <>
-      <span
-        {...attributes}
-        contentEditable={false}
-        className={`${
-          selected && focused ? 'shadow-md' : 'none'
-        } p-0.5 align-baseline inline-block rounded-sm font-semibold bg-sapien underline text-white text-xs`}
-        style={{ marginRight: '2px' }}
-      >
-        # {element.room.label}
-        {children}
-      </span>
-    </>
+    <span
+      {...attributes}
+      contentEditable={false}
+      className={`${selected && focused ? 'shadow-md' : 'none'} ${TAG_CLASS}`}
+      style={{ marginRight: '2px' }}
+    >
+      # {element.room.label} {children}
+    </span>
   );
 };
 
--- slatejs/components/UserMention.tsx
+++ slatejs/components/UserMention.tsx
@@ -1,23 +1,21 @@
 import { useSelected, useFocused } from 'slate-react';
 
+// helpers
+import { TAG_CLASS } from 'components/tribe/views/rooms/helpers';
+
 const UserMention = ({ attributes, children, element }) => {
   const focused = useFocused();
   const selected = useSelected();
 
   return (
-    <>
-      <span
-        {...attributes}
-        contentEditable={false}
-        className={`${
-          selected && focused ? 'shadow-md' : 'none'
-        } px-0.5 align-baseline inline-block font-semibold rounded-sm bg-sapien text-white text-xs`}
-        style={{ marginRight: '2px' }}
-      >
-        @{element.member.label}
-        {children}
-      </span>
-    </>
+    <span
+      {...attributes}
+      contentEditable={false}
+      className={`${selected && focused ? 'shadow-md' : 'none'} ${TAG_CLASS}`}
+      style={{ marginRight: '2px' }}
+    >
+      @{element.member.label} {children}
+    </span>
   );
 };
 
