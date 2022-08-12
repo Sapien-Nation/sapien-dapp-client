f40c7d6c8a1395c3e64bdff07668b9089150a201
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-08-12 17:56:24 -0500

    feat: Adding video support

--- tinymc/imageHandlers.tsx
+++ tinymc/imageHandlers.tsx
@@ -5,27 +5,24 @@ type Media = {
   url: string;
 };
 
-export const imageHandler = async (blobInfo, progress) => {
+export const imageHandler = async (blobInfo, ...rest) => {
+  console.log({ rest });
   const formData = new FormData();
+
   formData.append('file', blobInfo.blob());
 
   const fileData: Media = await uploadContentMedia(formData);
   return Promise.resolve(fileData.url);
 };
 
-export const filePicker = (cb) => {
+export const filePicker = (cb, value, meta) => {
   const input = document.createElement('input');
   input.setAttribute('type', 'file');
-  /*
-    Note: In modern browsers input[type="file"] is functional without
-    even adding it to the DOM, but that might not be the case in some older
-    or quirky browsers like IE, so you might want to add it to the DOM
-    just in case, and visually hide it. And do not forget do remove it
-    once you do not need it anymore.
-  */
+  // input.setAttribute('accept', 'image/*');
 
   input.onchange = function (event) {
     const file = (event.target as HTMLInputElement).files[0];
+
     const reader = new FileReader();
 
     reader.onload = async function () {
