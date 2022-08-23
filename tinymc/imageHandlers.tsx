1151c7837136aac8a330a516e3f94a13daf4e621
Ethan Escareño Rosano <ethan.rosanoo@gmail.com>
2022-08-22 20:52:18 -0500

    feat: fixing Video/Images on Expanded

--- tinymc/imageHandlers.tsx
+++ tinymc/imageHandlers.tsx
@@ -5,20 +5,19 @@ type Media = {
   url: string;
 };
 
-export const imageHandler = async (blobInfo, ...rest) => {
-  console.log({ rest });
-  const formData = new FormData();
-
-  formData.append('file', blobInfo.blob());
-
-  const fileData: Media = await uploadContentMedia(formData);
-  return Promise.resolve(fileData.url);
+export const imageHandler = async (blobInfo) => {
+  return Promise.resolve(blobInfo);
 };
 
 export const filePicker = (cb, value, meta) => {
   const input = document.createElement('input');
   input.setAttribute('type', 'file');
-  // input.setAttribute('accept', 'image/*');
+
+  if (meta.filetype === 'media') {
+    input.setAttribute('accept', 'video/* audio/*');
+  } else {
+    input.setAttribute('accept', 'image/*');
+  }
 
   input.onchange = function (event) {
     const file = (event.target as HTMLInputElement).files[0];
@@ -26,9 +25,13 @@ export const filePicker = (cb, value, meta) => {
     const reader = new FileReader();
 
     reader.onload = async function () {
-      const blobUrl = window.URL.createObjectURL(file);
+      const formData = new FormData();
+
+      formData.append('file', file);
+
+      const fileData: Media = await uploadContentMedia(formData);
       /* call the callback and populate the Title field with the file name */
-      cb(blobUrl, { title: file.name });
+      cb(fileData.url, { title: file.name });
     };
     reader.readAsDataURL(file);
   };
